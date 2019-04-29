f709486da092a1e5bbe47b7505f722b725cf3183
sknjpn <sknjpn@gmail.com>
2019-04-29 23:46:26 +0900

    Cell生成処理後、Rigidbodiesに追加するように

--- SyLife/CellManager.cpp
+++ SyLife/CellManager.cpp
@@ -1,6 +1,6 @@
 #include "CellManager.h"
 #include "Cell.h"
-
+#include "FieldManager.h"
 
 unique_ptr<CellManager>	g_cellManager;
 
@@ -15,5 +15,9 @@ CellManager::~CellManager()
 
 const shared_ptr<Cell>& CellManager::AddCell()
 {
-	return m_cells.emplace_back(make_shared<Cell>());
+	const auto& c = m_cells.emplace_back(make_shared<Cell>());
+	
+	g_fieldManager->m_rigidbodies.emplace_back(c);
+	
+	return c;
 }
