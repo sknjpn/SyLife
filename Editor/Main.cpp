0257db94f8e59b80cdc6e468cbbf9855e7acc243
sknjpn <sknjpn@gmail.com>
2019-06-07 22:11:42 +0900

    更新

--- Editor/Main.cpp
+++ Editor/Main.cpp
@@ -1,44 +1,44 @@
 ﻿struct Shape
 {
-	HSV		m_color;
-	Array<Vec2>	m_verticles;
+	s3d::HSV		m_color;
+	s3d::Array<s3d::Vec2>	m_verticles;
 
-	Shape(Array<Vec2> verticle)
+	Shape(s3d::Array<s3d::Vec2> verticle)
 		: m_verticles(verticle)
-		, m_color(RandomColor())
+		, m_color(s3d::RandomColor())
 	{}
 };
 
 void Main()
 {
 	// 最終的な図形
-	Array<Shape> shapes;
+	s3d::Array<Shape> shapes;
 
 	// 新規作成時の図形
-	Array<Vec2> verticles;
+	s3d::Array<s3d::Vec2> verticles;
 
-	Vec2* selectedVerticle = nullptr;
+	s3d::Vec2* selectedVerticle = nullptr;
 
-	while (System::Update())
+	while (s3d::System::Update())
 	{
-		ClearPrint();
+		s3d::ClearPrint();
 
 		int scale = 10;
 
 		const double thickness = 3.0 / scale;
-		const Vec2 boxSize = Vec2(5.0, 5.0) / scale;
-		auto t = Transformer2D(Mat3x2::Scale(scale).translated(Window::Size() / 2.0), true);
+		const s3d::Vec2 boxSize = s3d::Vec2(5.0, 5.0) / scale;
+		auto t = s3d::Transformer2D(s3d::Mat3x2::Scale(scale).translated(s3d::Window::Size() / 2.0), true);
 
-		Line(-Window::Size().x, 0, Window::Size().x, 0).draw(2.0 / scale, Palette::Red);
-		Line(0, -Window::Size().y, 0, Window::Size().y).draw(2.0 / scale, Palette::Red);
+		s3d::Line(-s3d::Window::Size().x, 0, s3d::Window::Size().x, 0).draw(2.0 / scale, s3d::Palette::Red);
+		s3d::Line(0, -s3d::Window::Size().y, 0, s3d::Window::Size().y).draw(2.0 / scale, s3d::Palette::Red);
 
-		for (int x = -Window::Size().x / scale; x < Window::Size().x / scale; x += 1) Line(x, -Window::Size().y, x, Window::Size().y).draw(1.0 / scale, ColorF(1.0, 0.25));
-		for (int y = -Window::Size().y / scale; y < Window::Size().y / scale; y += 1) Line(-Window::Size().x, y, Window::Size().x, y).draw(1.0 / scale, ColorF(1.0, 0.25));
+		for (int x = -s3d::Window::Size().x / scale; x < s3d::Window::Size().x / scale; x += 1) s3d::Line(x, -s3d::Window::Size().y, x, s3d::Window::Size().y).draw(1.0 / scale, s3d::ColorF(1.0, 0.25));
+		for (int y = -s3d::Window::Size().y / scale; y < s3d::Window::Size().y / scale; y += 1) s3d::Line(-s3d::Window::Size().x, y, s3d::Window::Size().x, y).draw(1.0 / scale, s3d::ColorF(1.0, 0.25));
 
-		Point cursor = Cursor::Pos();
-		RectF pointer(Arg::center(cursor), boxSize);
+		s3d::Point cursor = s3d::Cursor::Pos();
+		s3d::RectF pointer(s3d::Arg::center(cursor), boxSize);
 
-		Print << cursor;
+		s3d::Print << cursor;
 
 		// Draw
 		{
@@ -46,52 +46,52 @@ void Main()
 			{
 				// Face
 				{
-					Polygon p(s.m_verticles);
+					s3d::Polygon p(s.m_verticles);
 
-					p.draw(ColorF(s.m_color, 0.5));
+					p.draw(s3d::ColorF(s.m_color, 0.5));
 				}
 
 				// Line
 				for (auto it = s.m_verticles.begin(); it != s.m_verticles.end(); ++it)
 				{
-					Line l = (it == s.m_verticles.end() - 1) ? Line(s.m_verticles.front(), s.m_verticles.back()) : Line(*it, *(it + 1));
+					s3d::Line l = (it == s.m_verticles.end() - 1) ? s3d::Line(s.m_verticles.front(), s.m_verticles.back()) : s3d::Line(*it, *(it + 1));
 
-					l.draw(thickness, ColorF(s.m_color, 0.5));
+					l.draw(thickness, s3d::ColorF(s.m_color, 0.5));
 				}
 
 				// Verticle
 				for (auto it = verticles.begin(); it != verticles.end(); ++it)
 				{
-					auto r = RectF(Arg::center(*it), boxSize);
+					auto r = s3d::RectF(s3d::Arg::center(*it), boxSize);
 
-					r.draw(r.mouseOver() ? Palette::Blue : s.m_color);
+					r.draw(r.mouseOver() ? s3d::Palette::Blue : s.m_color);
 				}
 			}
 
 			if (verticles.empty())
 			{
 				// Pointer
-				pointer.draw(Palette::Red);
+				pointer.draw(s3d::Palette::Red);
 			}
 			else
 			{
 				// Line
 				for (auto it = verticles.begin(); it < verticles.end() - 1; ++it)
-					Line(*it, *(it + 1)).draw(thickness, ColorF(Palette::White, 0.5));
+					s3d::Line(*it, *(it + 1)).draw(thickness, s3d::ColorF(s3d::Palette::White, 0.5));
 
 				// Last Line
-				Line(verticles.back(), cursor).draw(thickness, ColorF(Palette::Gray, 0.5));
+				s3d::Line(verticles.back(), cursor).draw(thickness, s3d::ColorF(s3d::Palette::Gray, 0.5));
 
 				// Verticle
 				for (auto it = verticles.begin(); it != verticles.end(); ++it)
 				{
-					auto r = RectF(Arg::center(*it), boxSize);
+					auto r = s3d::RectF(s3d::Arg::center(*it), boxSize);
 
-					r.draw(r.mouseOver() ? Palette::Blue : Palette::White);
+					r.draw(r.mouseOver() ? s3d::Palette::Blue : s3d::Palette::White);
 				}
 
 				// Pointer
-				//pointer.draw(Palette::Red);
+				//pointer.draw(s3d::Palette::Red);
 			}
 		}
 
@@ -101,16 +101,16 @@ void Main()
 			{
 				for(auto it = shapes.rbegin(); it != shapes.rend(); ++it)
 				{
-					if (Polygon((*it).m_verticles).mouseOver())
+					if (s3d::Polygon((*it).m_verticles).mouseOver())
 					{
-						(*it).m_color.h += Mouse::Wheel();
+						(*it).m_color.h += s3d::Mouse::Wheel();
 
 						break;
 					}
 				}
 
 				// 新規配置
-				if (MouseL.down()) verticles.emplace_back(cursor);
+				if (s3d::MouseL.down()) verticles.emplace_back(cursor);
 			}
 			else
 			{
@@ -120,7 +120,7 @@ void Main()
 
 					for (auto it = verticles.begin(); it != verticles.end(); ++it)
 					{
-						auto r = RectF(Arg::center(*it), boxSize);
+						auto r = s3d::RectF(s3d::Arg::center(*it), boxSize);
 
 						if (r.leftClicked())
 						{
@@ -140,14 +140,14 @@ void Main()
 				}
 
 				// 連続配置
-				if (MouseL.down()) verticles.emplace_back(cursor);
-				if (MouseR.down()) verticles.pop_back();
+				if (s3d::MouseL.down()) verticles.emplace_back(cursor);
+				if (s3d::MouseR.down()) verticles.pop_back();
 			}
 		}
 
 		// Save
 		{
-			String s;
+			s3d::String s;
 
 			s += U"[";
 			s += U"\r  ";
@@ -155,7 +155,7 @@ void Main()
 			{
 				s += U"{\r    ";
 				{
-					s += Format(U"\"color\": [", (*it).m_color.toColor().r, U", ", (*it).m_color.toColor().g, U", ", (*it).m_color.toColor().b, U"]");
+					s += s3d::Format(U"\"color\": [", (*it).m_color.toColor().r, U", ", (*it).m_color.toColor().g, U", ", (*it).m_color.toColor().b, U"]");
 				}
 				s += U",";
 				s += U"\r    ";
@@ -164,7 +164,7 @@ void Main()
 					s += U"\"verticles\": [";
 					for (auto v = (*it).m_verticles.begin(); v != (*it).m_verticles.end(); ++v)
 					{
-						s += Format(U"[", (*v).x, U", ", (*v).y, U"]");
+						s += s3d::Format(U"[", (*v).x, U", ", (*v).y, U"]");
 
 						if (v != (*it).m_verticles.end() - 1) s += U", ";
 					}
@@ -179,7 +179,7 @@ void Main()
 			}
 			s += U"]";
 
-			TextWriter tw(U"a.json");
+			s3d::TextWriter tw(U"a.json");
 
 			tw.write(s);
 			tw.close();
--- Editor/stdafx.h
+++ Editor/stdafx.h
@@ -1,1 +1,12 @@
+# define NO_S3D_USING 
 # include <Siv3D.hpp> // OpenSiv3D v0.3.1
+
+//# include "sol.hpp"
+//# include "nanoflann.hpp"
+
+# include "Vector2D.h"
+
+using namespace std;
+//using namespace nanoflann;
+
+const double pi = 3.141592;
