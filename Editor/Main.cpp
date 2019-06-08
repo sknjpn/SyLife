2e2758b5261977105d51a45267f956d39ebb7eac
sknjpn <sknjpn@gmail.com>
2019-06-08 23:55:24 +0900

    SystemManagerの改善

--- Editor/Main.cpp
+++ Editor/Main.cpp
@@ -1,9 +1,11 @@
-﻿#include "Shape.h"
+﻿#include "SystemManager.h"
 
 void Main()
 {
+	g_systemManagerPtr = make_unique<SystemManager>();
 
 	while (s3d::System::Update())
 	{
+		g_systemManagerPtr->Update();
 	}
 }
