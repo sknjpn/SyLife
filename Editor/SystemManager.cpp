26e3683aa654df1e23db72cd8997556b5d2ec28d
sknjpn <sknjpn@gmail.com>
2019-06-09 00:00:14 +0900

    Manager初期化処理の追加

--- Editor/SystemManager.cpp
+++ Editor/SystemManager.cpp
@@ -1,11 +1,15 @@
 #include "SystemManager.h"
 #include "AssetManager.h"
+#include "ViewerManager.h"
+#include "FileManager.h"
 
 unique_ptr<SystemManager>	g_systemManagerPtr;
 
 SystemManager::SystemManager()
 {
 	g_assetManagerPtr = make_unique<AssetManager>();
+	g_viewerManagerPtr = make_unique<ViewerManager>();
+	g_fileManagerPtr = make_unique<FileManager>();
 }
 
 void SystemManager::Update()
