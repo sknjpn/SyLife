18cb57bbf7e42a290c8b872b885a8dc4f9ad8f60
sknjpn <sknjpn@gmail.com>
2019-06-09 01:19:27 +0900

    処理の改善

--- Editor/Molecule.h
+++ Editor/Molecule.h
@@ -5,4 +5,7 @@ class MoleculeModel
 public:
 	string	m_name;
 	string	m_filepath;
+	double	m_mass;
+	double	m_radius;
+	char	m_color[3];
 };
--- Editor/Part.h
+++ Editor/Part.h
@@ -5,4 +5,5 @@ class PartModel
 public:
 	string	m_name;
 	string	m_filepath;
+	double	m_mass;
 };
