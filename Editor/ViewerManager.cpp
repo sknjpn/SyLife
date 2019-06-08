e28b528309c1ae6e5a095661939b966308c3579f
sknjpn <sknjpn@gmail.com>
2019-06-09 00:46:23 +0900

    表示範囲の設定

--- Editor/ViewerManager.cpp
+++ Editor/ViewerManager.cpp
@@ -6,6 +6,10 @@ void ViewerManager::Update()
 {
 	for (const auto& v : m_viewers)
 	{
+		auto t = s3d::Transformer2D(s3d::Mat3x2::Translate(v->m_drawRect.pos), true);
+		
+		s3d::RectF(v->m_drawRect.size).drawFrame(4.0, 0.0, s3d::Palette::Red);
+
 		v->Update();
 	}
 }
