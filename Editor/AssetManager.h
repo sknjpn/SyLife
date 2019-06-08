5831d225716e555022761657bb5acf715c02fe88
sknjpn <sknjpn@gmail.com>
2019-06-09 01:08:35 +0900

    AssetManagerの内部追加

--- Editor/AssetManager.h
+++ Editor/AssetManager.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include "Molecule.h"
+#include "Part.h"
+#include "Cell.h"
+
 class AssetManager
 {
+public:
+	vector<shared_ptr<MoleculeModel>>	m_moleculeModels;
+	vector<shared_ptr<PartModel>>	m_partModels;
+	vector<shared_ptr<CellModel>>	m_cellModels;
+
 public:
 };
 
