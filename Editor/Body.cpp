3dfa258bc1851ea11ff77453d7cb6fe2eb7c8116
sknjpn <sknjpn@gmail.com>
2019-06-09 17:10:23 +0900

    更新

--- Editor/Body.cpp
+++ Editor/Body.cpp
@@ -1,1 +1,32 @@
 #include "Body.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
+#include <boost/optional.hpp>
+
+
+ptree BodyModel::ToJSON() const
+{
+	ptree pt;
+
+	pt.put("name", m_name);
+	pt.put("type", "Body");
+
+	ptree child;
+	{
+		ptree info;
+		info.put("id", 1);
+		info.put("name", "Alice");
+		child.push_back(std::make_pair("", info));
+	}
+	{
+		ptree info;
+		info.put("id", 2);
+		info.put("name", "Millia");
+		child.push_back(std::make_pair("", info));
+	}
+	pt.add_child("Data.info", child);
+
+	write_json("data_out.json", pt);
+}
+
