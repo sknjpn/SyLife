c1b37d93e3eb209c21c4f7be68d8bf7ba7063b3a
sknjpn <sknjpn@gmail.com>
2019-06-17 17:23:42 +0900

    update

--- Editor/Model.cpp
+++ Editor/Model.cpp
@@ -20,7 +20,7 @@ void Model::Save() const
 string Model::GetFilename() const
 {
 	auto filename = m_name + ".json";
-	auto f = [](unsigned char c) { return c == 0x20 ? 0x7F : tolower(c); };
+	auto f = [](unsigned char c) { return c == 0x20 ? 0x5f : tolower(c); };
 
 	transform(filename.begin(), filename.end(), filename.begin(), f);
 
