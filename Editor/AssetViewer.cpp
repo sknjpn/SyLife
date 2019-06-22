21398ef5dab781114e375ed8fd64680c955e47dd
sknjpn <sknjpn@gmail.com>
2019-06-22 22:31:45 +0900

    AssetViewerの改善

--- Editor/AssetViewer.cpp
+++ Editor/AssetViewer.cpp
@@ -3,95 +3,21 @@
 #include "CellModel.h"
 #include "MoleculeModel.h"
 #include "PartModel.h"
+#include "BodyModel.h"
+#include "EquipmentModel.h"
+#include "ModuleModel.h"
 
 void AssetViewer::Update()
 {
-	static s3d::Font font14(13);
-	static s3d::Font font12(10, s3d::Typeface::Bold);
+	m_drawPos = s3d::Vec2::Zero();
 
-	s3d::Vec2 pos(0.0, 0.0);
-
-	// Cell
-	{
-		{
-			auto t = s3d::Transformer2D(s3d::Mat3x2::Translate(pos), true);
-
-			font14(U"Cell").draw();
-			pos.moveBy(0.0, 16.0);
-		}
-
-		pos.moveBy(16.0, 0.0);
-		const auto& models = g_assetManagerPtr->GetModels<CellModel>();
-		for (auto it = models.begin(); it != models.end(); ++it)
-		{
-			auto t = s3d::Transformer2D(s3d::Mat3x2::Translate(pos), true);
-			auto f = font12(s3d::Unicode::Widen((*it)->m_name));
-
-			f.region().draw(m_selectedModel == *it ? s3d::Palette::Darkorange : s3d::ColorF(1.0, f.region().mouseOver() ? 0.5 : 0.0));
-			f.draw();
-
-			if (f.region().leftClicked()) SetSelectedModel(*it);
-
-			pos.moveBy(0.0, 15.0);
-		}
-		pos.moveBy(-16.0, 0.0);
-	}
-
-	// Molecule
-	{
-		{
-			auto t = s3d::Transformer2D(s3d::Mat3x2::Translate(pos), true);
-
-			font14(U"Molecule").draw();
-			pos.moveBy(0.0, 16.0);
-		}
-
-		pos.moveBy(16.0, 0.0);
-		const auto& models = g_assetManagerPtr->GetModels<MoleculeModel>();
-		for (auto it = models.begin(); it != models.end(); ++it)
-		{
-			auto t = s3d::Transformer2D(s3d::Mat3x2::Translate(pos), true);
-			auto f = font12(s3d::Unicode::Widen((*it)->m_name));
-
-			f.region().draw(m_selectedModel == *it ? s3d::Palette::Darkorange : s3d::ColorF(1.0, f.region().mouseOver() ? 0.5 : 0.0));
-			f.draw();
-
-			if (f.region().leftClicked()) SetSelectedModel(*it);
-
-			pos.moveBy(0.0, 15.0);
-		}
-		pos.moveBy(-16.0, 0.0);
-	}
-
-	// Part
-	{
-		{
-			auto t = s3d::Transformer2D(s3d::Mat3x2::Translate(pos), true);
-
-			font14(U"Part").draw();
-			pos.moveBy(0.0, 16.0);
-		}
-
-		pos.moveBy(16.0, 0.0);
-		const auto& models = g_assetManagerPtr->GetModels<PartModel>();
-		for (auto it = models.begin(); it != models.end(); ++it)
-		{
-			auto t = s3d::Transformer2D(s3d::Mat3x2::Translate(pos), true);
-			auto f = font12(s3d::Unicode::Widen((*it)->m_name));
-
-			f.region().draw(m_selectedModel == *it ? s3d::Palette::Darkorange : s3d::ColorF(1.0, f.region().mouseOver() ? 0.5 : 0.0));
-			f.draw();
-
-			if (f.region().leftClicked()) SetSelectedModel(*it);
-
-			pos.moveBy(0.0, 15.0);
-		}
-		pos.moveBy(-16.0, 0.0);
-	}
+	DrawModels<CellModel>("CellModel");
+	DrawModels<MoleculeModel>("MoleculeModel");
+	DrawModels<BodyModel>("BodyModel");
+	DrawModels<EquipmentModel>("EquipmentModel");
+	DrawModels<ModuleModel>("ModuleModel");
 
 	// Save
 	if (s3d::KeyControl.pressed() && s3d::KeyS.down() && m_selectedModel != nullptr)
-	{
 		write_json(m_selectedModel->GetFilepath(), m_selectedModel->Save());
-	}
 }
--- Editor/AssetViewer.h
+++ Editor/AssetViewer.h
@@ -7,6 +7,7 @@ class AssetViewer
 	: public Viewer
 {
 	shared_ptr<Model>	m_selectedModel;
+	s3d::Vec2	m_drawPos;
 
 public:
 	AssetViewer()
@@ -16,5 +17,39 @@ public:
 
 	void	Update() override;
 	void	SetSelectedModel(const shared_ptr<Model>& model);
+
+	template <typename T>
+	void	DrawModels(const string& text)
+	{
+		static s3d::Font font13(13);
+		static s3d::Font font10(10, s3d::Typeface::Bold);
+
+		// Type
+		{
+			auto t = s3d::Transformer2D(s3d::Mat3x2::Translate(m_drawPos), true);
+
+			font13(s3d::Unicode::Widen(text)).draw();
+			m_drawPos.moveBy(0.0, 16.0);
+		}
+
+		// Models
+		{
+			m_drawPos.moveBy(16.0, 0.0);
+			const auto& models = g_assetManagerPtr->GetModels<T>();
+			for (auto it = models.begin(); it != models.end(); ++it)
+			{
+				auto t = s3d::Transformer2D(s3d::Mat3x2::Translate(m_drawPos), true);
+				auto f = font10(s3d::Unicode::Widen((*it)->m_name));
+
+				f.region().draw(m_selectedModel == *it ? s3d::Palette::Darkorange : s3d::ColorF(1.0, f.region().mouseOver() ? 0.5 : 0.0));
+				f.draw();
+
+				if (f.region().leftClicked()) SetSelectedModel(*it);
+
+				m_drawPos.moveBy(0.0, 15.0);
+			}
+			m_drawPos.moveBy(-16.0, 0.0);
+		}
+	}
 };
 
