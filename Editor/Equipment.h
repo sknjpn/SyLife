ae8cf3b56f7161d62605f6f48ecb0228c521dead
sknjpn <sknjpn@gmail.com>
2019-06-23 23:07:04 +0900

    不要なFilepathの削除

--- Editor/Equipment.h
+++ Editor/Equipment.h
@@ -16,8 +16,6 @@ public:
 	ptree	AddToJSON(ptree pt) const;
 	void	SetFromJSON(const ptree& pt);
 
-	string	GetFilepath() const override { return "assets/part/" + GetFilename(); }
-
 	void	Load(const ptree& pt) override { SetFromJSON(pt); }
 	ptree	Save() const override { return AddToJSON(ptree()); }
 };
