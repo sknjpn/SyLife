5455aaaa1b948b5116a5f712cbbb603764fa84c6
sknjpn <sknjpn@gmail.com>
2019-06-26 02:21:51 +0900

    stdafxの変更

--- SyLife/stdafx.h
+++ SyLife/stdafx.h
@@ -1,10 +1,11 @@
-# define NO_S3D_USING 
-# include <Siv3D.hpp> // OpenSiv3D v0.3.1
+#define NO_S3D_USING 
+#include <Siv3D.hpp> // OpenSiv3D v0.3.1
 
-# include "sol.hpp"
-# include "nanoflann.hpp"
+#include "sol.hpp"
+#include "nanoflann.hpp"
 
-# include "Vector2D.h"
+#include "Random.h"
+#include "Vector2D.h"
 
 using namespace std;
 using namespace nanoflann;
