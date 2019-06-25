696417a0e2eb2005a89b92da74b00ef8244ef4d0
sknjpn <sknjpn@gmail.com>
2019-06-26 01:51:07 +0900

    不要クラスの削除

--- SyLife/RecipeManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "RecipeManager.h"
-
-unique_ptr<RecipeManager>	g_recipeManagerPtr;
-
-const shared_ptr<Recipe>& RecipeManager::AddRecipe()
-{
-	return m_recipes.emplace_back(make_shared<Recipe>());
-}
