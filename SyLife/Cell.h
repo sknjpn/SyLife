9879db322fc7a8e83c570b20aa8b1014126e397f
sknjpn <sknjpn@gmail.com>
2019-06-26 01:10:31 +0900

    インデント修正

--- SyLife/Cell.h
+++ SyLife/Cell.h
@@ -59,7 +59,7 @@ public:
 
 	shared_ptr<BodyState>	m_body;
 	vector<shared_ptr<EquipmentState>>	m_equipments;
-	vector<shared_ptr<ModuleState>>	m_modules;
+	vector<shared_ptr<ModuleState>>		m_modules;
 
 	vector<shared_ptr<PartState>>		m_partStates;
 
