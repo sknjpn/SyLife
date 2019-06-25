f4633aa72597a53022cc1bb752270112d7532c33
sknjpn <sknjpn@gmail.com>
2019-06-25 23:28:19 +0900

    moleculeを統合

--- Editor/Molecule.h
+++ Editor/Molecule.h
@@ -2,6 +2,7 @@
 
 #include "Model.h"
 #include "Viewer.h"
+#include "ViewerManager.h"
 
 class MoleculeViewer;
 
@@ -43,3 +44,57 @@ public:
 	void	Update() override;
 };
 
+inline void MoleculeModel::MakeViewers()
+{
+	g_viewerManagerPtr->AddViewer<MoleculeViewer>(dynamic_pointer_cast<MoleculeModel>(shared_from_this()));
+}
+
+inline void MoleculeModel::AddToJSON(ptree& pt) const
+{
+	// mass
+	pt.put<double>("mass", m_mass);
+
+	// color
+	{
+		ptree color;
+
+		color.put("r", m_color.r);
+		color.put("g", m_color.g);
+		color.put("b", m_color.b);
+
+		pt.add_child("color", color);
+	}
+
+	Model::AddToJSON(pt);
+
+	pt.put("type", "MoleculeModel");
+}
+
+inline void MoleculeModel::SetFromJSON(const ptree& pt)
+{
+	// mass
+	m_mass = pt.get<double>("mass");
+
+	// color
+	m_color = s3d::Color(pt.get<int>("color.r"), pt.get<int>("color.g"), pt.get<int>("color.b"));
+
+	// radius
+	m_radius = sqrt(m_mass);
+
+	Model::SetFromJSON(pt);
+}
+
+inline void MoleculeViewer::Update()
+{
+	// name
+	{
+		s3d::SimpleGUI::TextBox(m_textEditState_name, s3d::Vec2(10, 10), 240);
+		m_model->m_name = s3d::Unicode::Narrow(m_textEditState_name.text);
+	}
+
+	// mass
+	{
+		s3d::SimpleGUI::TextBox(m_textEditState_mass, s3d::Vec2(10, 50), 240);
+		m_model->m_mass = s3d::Parse<double>(m_textEditState_mass.text);
+	}
+}
