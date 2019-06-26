a0901f7eafd3d4cb7fd3eb1cb125c34aeec318bd
sknjpn <sknjpn@gmail.com>
2019-06-27 02:30:26 +0900

    PartPaletteViewerの追加

---
+++ SyLife/PartPaletteViewer.cpp
@@ -0,0 +1,16 @@
+#include "PartPaletteViewer.h"
+
+#include "Body.h"
+#include "Equipment.h"
+#include "Module.h"
+
+shared_ptr<PartModel>	PartPaletteViewer::m_selectedPart;
+
+void PartPaletteViewer::Update()
+{
+	m_drawPos = s3d::Vec2::Zero();
+
+	DrawModels<BodyModel>("BodyModel");
+	DrawModels<EquipmentModel>("EquipmentModel");
+	DrawModels<ModuleModel>("ModuleModel");
+}
