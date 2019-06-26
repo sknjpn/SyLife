ae565b4c6ad88fedd23df961ad1ccca4a518d5c5
sknjpn <sknjpn@gmail.com>
2019-06-26 21:18:41 +0900

    削除していなかった問題の修正

--- SyLife/SystemManager.cpp
+++ SyLife/SystemManager.cpp
@@ -3,7 +3,6 @@
 #include "AssetManager.h"
 #include "MoleculeManager.h"
 #include "CellManager.h"
-#include "RecipeManager.h"
 
 unique_ptr<SystemManager>	g_systemManagerPtr;
 
@@ -13,7 +12,6 @@ SystemManager::SystemManager()
 	g_cellManagerPtr = make_unique<CellManager>();
 	g_fieldManagerPtr = make_unique<FieldManager>();
 	g_moleculeManagerPtr = make_unique<MoleculeManager>();
-	g_recipeManagerPtr = make_unique<RecipeManager>();
 
 	g_assetManagerPtr->Init();
 	g_fieldManagerPtr->Init();
