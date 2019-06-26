016e69f6bd4e3fef6ee9b2047cfec0bb31ade5e1
sknjpn <sknjpn@gmail.com>
2019-06-27 00:04:41 +0900

    Titleの表現追加

--- SyLife/SystemManager_Update.cpp
+++ SyLife/SystemManager_Update.cpp
@@ -1,10 +1,14 @@
 #include "SystemManager.h"
 #include "FieldManager.h"
+#include "ViewerManager.h"
 
 void SystemManager::Update()
 {
+	g_viewerManagerPtr->Update();
+	/*
 	auto f = s3d::KeySpace.pressed() ? 5 : 1;
 
 	for (int i = 0; i < f; ++i)
 		g_fieldManagerPtr->Update();
+	*/
 }
--- SyLife/TitleViewer.cpp
+++ SyLife/TitleViewer.cpp
@@ -2,29 +2,34 @@
 
 void TitleViewer::Init()
 {
-	for (int i = 0; i < 100; ++i)
-	{
-		auto& b = m_bubbles.emplace_back();
 
-		b.m_position = s3d::RandomVec3(100.0).moveBy(0.0, 0.0, 100.0);
-	}
 }
 
 void TitleViewer::Update()
 {
-	// bubbles
-	for (const auto& b : m_bubbles)
 	{
+		auto& b = m_bubbles.emplace_back();
+
+		b.m_position = s3d::Vec3(s3d::Random(-50.0, 50.0), -50.0, 100+s3d::Random(-50.0, 50.0));
+	}
+
+	// bubbles
+	for (auto& b : m_bubbles)
+	{
+		b.m_position.y += 0.5;
+
 		// draw
 		{
 			s3d::Vec3 camPos(0.0, 0.0, 0.0);
 			s3d::Vec3 p = b.m_position - camPos;
 
-			auto x = asin(p.x / p.z) / (3.14 / 3.0) * s3d::Window::Size().x;
-			auto y = asin(p.y / p.z) / (3.14 / 3.0) * s3d::Window::Size().y;
-			auto r = 10.0 / p.length();
+			auto x = (asin(p.x / p.z) / (3.14 / 3.0) + 0.5) * s3d::Window::Size().x;
+			auto y = (-asin(p.y / p.z) / (3.14 / 3.0) + 0.5) * s3d::Window::Size().y;
+			auto r = 1000.0 / p.length();
 
 			s3d::Circle(x, y, r).draw(s3d::Palette::Lightblue).drawFrame(1.0, s3d::Palette::Black);
 		}
 	}
+
+	m_bubbles.remove_if([](const auto& b) { return b.m_position.y > 50.0; });
 }
