a9cd7c28fcbbac406278e1ce2f7527cfe2d26cf4
sknjpn <sknjpn@gmail.com>
2019-06-28 22:37:43 +0900

    コード整理

--- Editor/ViewerManager.h
+++ Editor/ViewerManager.h
@@ -22,10 +22,7 @@ public:
 	bool	HasViewer() const { return GetViewer<T>() != nullptr; }
 
 	template <typename T>
-	bool	DeleteViewer() const
-	{
-		m_viewers.erase(remove_if(m_viewers.begin(), m_viewers.end(), [](const auto& v) { return dynamic_pointer_cast<T>(v) != nullptr; }), m_viewers.end());
-	}
+	bool	DeleteViewer() const { m_viewers.erase(remove_if(m_viewers.begin(), m_viewers.end(), [](const auto& v) { return dynamic_pointer_cast<T>(v) != nullptr; }), m_viewers.end()); }
 };
 
 extern unique_ptr<ViewerManager>	g_viewerManagerPtr;
