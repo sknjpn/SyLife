9750ffe19e6b748c479d70f382661ec8d6ebbfe1
sknjpn <sknjpn@gmail.com>
2019-06-28 21:46:27 +0900

    EggManagerの詳細追加

--- SyLife/EggManager.cpp
+++ SyLife/EggManager.cpp
@@ -1,12 +1,27 @@
-#include "stdafx.h"
 #include "EggManager.h"
+#include "Egg.h"
+#include "Cell.h"
+#include "FieldManager.h"
 
+unique_ptr<EggManager> g_eggManagerPtr;
 
-EggManager::EggManager()
+Indexer<EggState>& EggManager::GetIndexer()
 {
+	return m_indexer;
 }
 
-
-EggManager::~EggManager()
+const Indexer<EggState>& EggManager::GetIndexer() const
 {
+	return m_indexer;
+}
+
+const shared_ptr<EggState>& EggManager::AddEggState(const shared_ptr<CellModel>& cellModel)
+{
+	const auto& m = GetEggStates().emplace_back(make_shared<EggState>());
+
+	m->m_model = model;
+	m->m_radius = model->m_radius;
+	m->m_mass = model->m_mass;
+
+	return m;
 }
--- SyLife/EggManager.h
+++ SyLife/EggManager.h
@@ -1,8 +1,26 @@
 #pragma once
+
+#include "Indexer.h"
+
+class EggState;
+class CellModel;
+
 class EggManager
 {
+	Indexer<EggState>	m_indexer;
+
 public:
-	EggManager();
-	~EggManager();
+	const shared_ptr<EggState>&	AddEggState(const shared_ptr<CellModel>& cellModel);
+
+	vector<shared_ptr<EggState>>&	GetEggStates() { return m_indexer.m_cloud.m_particles; }
+	const vector<shared_ptr<EggState>>&	GetEggStates() const { return m_indexer.m_cloud.m_particles; }
+
+	Indexer<EggState>&	GetIndexer();
+	const Indexer<EggState>&	GetIndexer() const;
+
+
+	void	Update();
+	void	Draw();
 };
 
+extern unique_ptr<EggManager> g_eggManagerPtr;
