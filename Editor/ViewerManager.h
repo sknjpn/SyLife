c34685eac12ab3f66edcda17fccd84c9ded467b5
sknjpn <sknjpn@gmail.com>
2019-06-28 23:14:11 +0900

    問題の修正

--- Editor/ViewerManager.h
+++ Editor/ViewerManager.h
@@ -16,7 +16,7 @@ public:
 	shared_ptr<T>	AddViewer(Args&& ...args) { return dynamic_pointer_cast<T>(m_viewers.emplace_back(make_shared<T>(args...))); }
 
 	template <typename T>
-	const shared_ptr<T>&	GetViewer() const;
+	shared_ptr<T>	GetViewer() const;
 
 	template <typename T>
 	bool	HasViewer() const { return GetViewer<T>() != nullptr; }
@@ -28,7 +28,7 @@ public:
 extern unique_ptr<ViewerManager>	g_viewerManagerPtr;
 
 template<typename T>
-inline const shared_ptr<T>& ViewerManager::GetViewer() const
+inline shared_ptr<T> ViewerManager::GetViewer() const
 {
 	for (const auto& v : m_viewers)
 		if (dynamic_pointer_cast<T>(v) != nullptr) return dynamic_pointer_cast<T>(v);
