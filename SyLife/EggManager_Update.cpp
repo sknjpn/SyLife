ed5fac1462ea1b03e3243cce2f47c53030f2f52b
sknjpn <sknjpn@gmail.com>
2019-06-28 22:08:33 +0900

    eggの孵化処理の追加

--- SyLife/EggManager_Update.cpp
+++ SyLife/EggManager_Update.cpp
@@ -1,25 +1,40 @@
 #include "EggManager.h"
 #include "FieldManager.h"
 #include "AssetManager.h"
+#include "CellManager.h"
+#include "Cell.h"
 #include "Egg.h"
+#include "Random.h"
+
+#include <boost/math/constants/constants.hpp>
 
 void EggManager::Update()
 {
-	for (const auto& m : GetEggStates())
+	for (const auto& e : GetEggStates())
 	{
 		// •Ài‰^“®
-		m->m_position += m->m_velocity;
+		e->m_position += e->m_velocity;
 
 		// •Ç‚Å‚Ì”½ŽË
-		if (m->m_position.m_x < 0 && m->m_velocity.m_x < 0) m->m_velocity.m_x = -m->m_velocity.m_x;
-		if (m->m_position.m_y < 0 && m->m_velocity.m_y < 0) m->m_velocity.m_y = -m->m_velocity.m_y;
-		if (m->m_position.m_x > g_fieldManagerPtr->GetSize().m_x && m->m_velocity.m_x > 0) m->m_velocity.m_x = -m->m_velocity.m_x;
-		if (m->m_position.m_y > g_fieldManagerPtr->GetSize().m_y && m->m_velocity.m_y > 0) m->m_velocity.m_y = -m->m_velocity.m_y;
+		if (e->m_position.m_x < 0 && e->m_velocity.m_x < 0) e->m_velocity.m_x = -e->m_velocity.m_x;
+		if (e->m_position.m_y < 0 && e->m_velocity.m_y < 0) e->m_velocity.m_y = -e->m_velocity.m_y;
+		if (e->m_position.m_x > g_fieldManagerPtr->GetSize().m_x && e->m_velocity.m_x > 0) e->m_velocity.m_x = -e->m_velocity.m_x;
+		if (e->m_position.m_y > g_fieldManagerPtr->GetSize().m_y && e->m_velocity.m_y > 0) e->m_velocity.m_y = -e->m_velocity.m_y;
 
 		// –€ŽC’ïR
-		m->m_velocity *= 0.95;
+		e->m_velocity *= 0.95;
+
+		// ›z‰»
+		if (RandomBool(0.01))
+		{
+			e->m_isDestroyed = true;
+			
+			const auto& c = g_cellManagerPtr->AddCellState(e->m_cellModel);
+			c->m_position = e->m_position;
+			c->m_rotation = Random(boost::math::constants::pi<double>() * 2.0);
+		}
 	}
 
-	GetEggStates().erase(remove_if(GetEggStates().begin(), GetEggStates().end(), [](const auto& m) { return m->m_isDestroyed; }), GetEggStates().end());
+	GetEggStates().erase(remove_if(GetEggStates().begin(), GetEggStates().end(), [](const auto& e) { return e->m_isDestroyed; }), GetEggStates().end());
 	m_indexer.Update();
 }
