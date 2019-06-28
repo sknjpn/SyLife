2a106ad09b2164366acac718d667310903da564c
sknjpn <sknjpn@gmail.com>
2019-06-28 22:33:34 +0900

    HasViewerとGetViewerの追加

--- Editor/ViewerManager.h
+++ Editor/ViewerManager.h
@@ -14,6 +14,18 @@ public:
 
 	template <typename T, typename... Args>
 	shared_ptr<T>	AddViewer(Args&& ...args) { return dynamic_pointer_cast<T>(m_viewers.emplace_back(make_shared<T>(args...))); }
+
+	template <typename T>
+	const shared_ptr<T>&	GetViewer() const
+	{
+		for (const auto& v : m_viewers)
+			if (dynamic_pointer_cast<T>(v) != nullptr) return dynamic_pointer_cast<T>(v);
+
+		return nullptr;
+	}
+
+	template <typename T>
+	bool	HasViewer() const { return GetViewer<T>() != nullptr; }
 };
 
 extern unique_ptr<ViewerManager>	g_viewerManagerPtr;
