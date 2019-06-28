4e02296bc3abcd0a4d5535dcba911dbe1b3fe72b
sknjpn <sknjpn@gmail.com>
2019-06-28 22:34:41 +0900

    コードの分離

--- Editor/ViewerManager.h
+++ Editor/ViewerManager.h
@@ -16,16 +16,19 @@ public:
 	shared_ptr<T>	AddViewer(Args&& ...args) { return dynamic_pointer_cast<T>(m_viewers.emplace_back(make_shared<T>(args...))); }
 
 	template <typename T>
-	const shared_ptr<T>&	GetViewer() const
-	{
-		for (const auto& v : m_viewers)
-			if (dynamic_pointer_cast<T>(v) != nullptr) return dynamic_pointer_cast<T>(v);
-
-		return nullptr;
-	}
+	const shared_ptr<T>&	GetViewer() const;
 
 	template <typename T>
 	bool	HasViewer() const { return GetViewer<T>() != nullptr; }
 };
 
 extern unique_ptr<ViewerManager>	g_viewerManagerPtr;
+
+template<typename T>
+inline const shared_ptr<T>& ViewerManager::GetViewer() const
+{
+	for (const auto& v : m_viewers)
+		if (dynamic_pointer_cast<T>(v) != nullptr) return dynamic_pointer_cast<T>(v);
+
+	return nullptr;
+}
