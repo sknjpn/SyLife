7b614537d26c87032a31bb818dc3988330d31808
sknjpn <sknjpn@gmail.com>
2019-06-29 15:08:56 +0900

    ヘッダに記述するように変更

--- Editor/Molecule.h
+++ Editor/Molecule.h
@@ -105,3 +105,17 @@ inline void MoleculeViewer::Update()
 		m_model->m_mass = s3d::Parse<double>(m_textEditState_mass.text);
 	}
 }
+
+inline void MoleculeModel::SetFromJSON(const ptree& pt)
+{
+	// mass
+	m_mass = pt.get<double>("mass");
+
+	// color
+	m_color = s3d::Color(pt.get<int>("color.r"), pt.get<int>("color.g"), pt.get<int>("color.b"));
+
+	// radius
+	m_radius = sqrt(m_mass);
+
+	Model::SetFromJSON(pt);
+}
