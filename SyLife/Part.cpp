c55969356e9c7414061bdd3d492d676777e4f1a6
sknjpn <sknjpn@gmail.com>
2019-06-29 15:19:06 +0900

    不要なメンバの削除

--- SyLife/Part.cpp
+++ SyLife/Part.cpp
@@ -3,11 +3,23 @@
 
 s3d::RectF PartModel::GetApproximateRect() const
 {
-	return s3d::RectF(
-		m_approximateRect.first.m_x,
-		m_approximateRect.first.m_y,
-		m_approximateRect.second.m_x - m_approximateRect.first.m_x,
-		m_approximateRect.second.m_y - m_approximateRect.first.m_y);
+	double tlx = m_shapes.front().m_polygon.vertices().front().x;
+	double tly = m_shapes.front().m_polygon.vertices().front().y;
+	double brx = m_shapes.front().m_polygon.vertices().front().x;
+	double bry = m_shapes.front().m_polygon.vertices().front().y;
+
+	for (const auto& s : m_shapes)
+	{
+		for (const auto& v : s.m_polygon.vertices())
+		{
+			if (tlx > v.x) tlx = v.x;
+			if (tly > v.y) tly = v.y;
+			if (brx < v.x) brx = v.x;
+			if (bry < v.y) bry = v.y;
+		}
+	}
+
+	return s3d::RectF(tlx, tly, brx - tlx, bry - tly);
 }
 
 double PartModel::GetRectInertia() const
@@ -26,30 +38,6 @@ void PartModel::SetFromJSON(const ptree& pt)
 	// shapes
 	for (auto shape : pt.get_child("shapes"))
 		m_shapes.emplace_back().SetFromJSON(shape.second);
-
-	// approximateRect
-	{
-		auto& afx = m_approximateRect.first.m_x;
-		auto& afy = m_approximateRect.first.m_y;
-		auto& asx = m_approximateRect.second.m_x;
-		auto& asy = m_approximateRect.second.m_y;
-
-		afx = m_shapes.front().m_polygon.vertices().front().x;
-		afy = m_shapes.front().m_polygon.vertices().front().y;
-		asx = m_shapes.front().m_polygon.vertices().front().x;
-		asy = m_shapes.front().m_polygon.vertices().front().y;
-
-		for (const auto& s : m_shapes)
-		{
-			for (const auto& v : s.m_polygon.vertices())
-			{
-				if (afx > v.x) afx = v.x;
-				if (afy > v.y) afy = v.y;
-				if (asx < v.x) asx = v.x;
-				if (asy < v.y) asy = v.y;
-			}
-		}
-	}
 
 	Model::SetFromJSON(pt);
 }
--- SyLife/Part.h
+++ SyLife/Part.h
@@ -17,9 +17,13 @@ class PartModel
 	double	m_mass;
 	Storage	m_material;
 	vector<ShapeModel>	m_shapes;
-	pair<Vector2D, Vector2D>	m_approximateRect;
 
 public:
+	// Get
+	double	GetMass() const { return m_mass; }
+	const Storage&	GetMaterial() const { return m_material; }
+	const vector<ShapeModel>&	GetShapes() const { return m_shapes; }
+
 	virtual shared_ptr<PartState>	MakeState() = 0;
 
 	void	Draw(double a = 0.5) { for (const auto& s : m_shapes) s.Draw(a); }
@@ -28,6 +32,7 @@ public:
 	double		GetRectInertia() const;
 	Vector2D	GetCenter() const { return (m_approximateRect.first + m_approximateRect.second) / 2.0; }
 
+	// JSON
 	void	SetFromJSON(const ptree& pt);
 	void	Load(const ptree& pt) override { SetFromJSON(pt); }
 };
