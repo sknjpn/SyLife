58d025d098ade791b5a97fa44e061c85c31b34ea
sknjpn <sknjpn@gmail.com>
2019-06-29 14:14:30 +0900

    StorageのEditor実装

---
+++ Editor/Storage.cpp
@@ -0,0 +1,82 @@
+#include "Storage.h"
+#include "AssetManager.h"
+#include "MoleculeManager.h"
+
+void Storage::AddMoleculeState(const shared_ptr<MoleculeModel>& model)
+{
+	for (auto it = m_molecules.begin(); it != m_molecules.end(); ++it)
+	{
+		if ((*it).first == model)
+		{
+			++(*it).second;
+
+			return;
+		}
+	}
+
+	m_molecules.emplace_back(model, 1);
+}
+
+void Storage::AddMoleculeState(const shared_ptr<MoleculeModel>& model, int size)
+{
+	for (auto it = m_molecules.begin(); it != m_molecules.end(); ++it)
+	{
+		if ((*it).first == model)
+		{
+			(*it).second += size;
+
+			return;
+		}
+	}
+
+	m_molecules.emplace_back(model, size);
+}
+
+int Storage::NumMolecule(const shared_ptr<MoleculeModel>& model) const
+{
+	for (auto it = m_molecules.begin(); it != m_molecules.end(); ++it)
+	{
+		if ((*it).first == model) return (*it).second;
+	}
+
+	return 0;
+}
+
+int Storage::NumMolecule(const string& name) const
+{
+	return NumMolecule(g_assetManagerPtr->GetModel<MoleculeModel>(name));
+}
+
+void Storage::PullMolecule(const shared_ptr<MoleculeModel>& model)
+{
+	for (auto it = m_molecules.begin(); it != m_molecules.end(); ++it)
+	{
+		if ((*it).first == model)
+		{
+			if (--(*it).second <= 0) m_molecules.erase(it);
+
+			return;
+		}
+	}
+}
+
+void Storage::PullMolecule(const shared_ptr<MoleculeModel>& model, int size)
+{
+	for (auto it = m_molecules.begin(); it != m_molecules.end(); ++it)
+	{
+		if ((*it).first == model)
+		{
+			if (((*it).second -= size) <= 0) m_molecules.erase(it);
+
+			return;
+		}
+	}
+}
+
+void Storage::AddStorage(const Storage& storage)
+{
+	for (const auto& m : storage.m_molecules)
+	{
+		AddMoleculeState(m.first, m.second);
+	}
+}
---
+++ Editor/Storage.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Molecule.h"
+
+class Storage
+{
+public:
+	vector<pair<shared_ptr<MoleculeModel>, int>>	m_molecules;
+
+public:
+	void	AddMoleculeState(const shared_ptr<MoleculeModel>& model);
+	void	AddMoleculeState(const shared_ptr<MoleculeModel>& model, int size);
+	int		NumMolecule(const shared_ptr<MoleculeModel>& model) const;
+	int		NumMolecule(const string& name) const;
+	void	PullMolecule(const shared_ptr<MoleculeModel>& model);
+	void	PullMolecule(const shared_ptr<MoleculeModel>& model, int size);
+	void	Clear() { m_molecules.clear(); }
+	void	AddStorage(const Storage& storage);
+};
+
