106ef47daf840a3bdfef2802af22c1c79a259369
sknjpn <sknjpn@gmail.com>
2019-06-29 15:49:19 +0900

    StorageのModel化

--- Editor/Storage.h
+++ Editor/Storage.h
@@ -1,8 +1,11 @@
 #pragma once
 
-#include "Molecule.h"
+#include "Model.h"
+
+class MoleculeModel;
 
 class Storage
+	: public Model
 {
 public:
 	vector<pair<shared_ptr<MoleculeModel>, unsigned int>>	m_molecules;
@@ -14,4 +17,10 @@ public:
 	void	PullMolecule(const shared_ptr<MoleculeModel>& model, unsigned int size = 1);
 	void	Clear() { m_molecules.clear(); }
 	void	AddStorage(const Storage& storage);
+
+	// JSON
+	void	SetFromJSON(const ptree& pt);
+	void	Load(const ptree& pt) override { SetFromJSON(pt); }
+	void	AddToJSON(ptree& pt) const;
+	void	Save(ptree& pt) const override { AddToJSON(pt); }
 };
