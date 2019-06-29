2793ed3ee43cdb176a467fe0a17059635cac9cbe
sknjpn <sknjpn@gmail.com>
2019-06-29 16:54:18 +0900

    関数追加

--- SyLife/Storage.h
+++ SyLife/Storage.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include "Model.h"
+#include "AssetManager.h"
 #include "Molecule.h"
 
+class MoleculeModel;
+
 class Storage
+	: public Model
 {
 public:
 	vector<pair<shared_ptr<MoleculeModel>, unsigned int>>	m_molecules;
@@ -11,7 +16,55 @@ public:
 	void	AddMoleculeState(const shared_ptr<MoleculeModel>& model, unsigned int size = 1);
 	int		NumMolecule(const shared_ptr<MoleculeModel>& model) const;
 	int		NumMolecule(const string& name) const;
+	bool	HasMolecule(const Storage& storage);
 	void	PullMolecule(const shared_ptr<MoleculeModel>& model, unsigned int size = 1);
+	void	PullMolecule(const Storage& storage);
 	void	Clear() { m_molecules.clear(); }
 	void	AddStorage(const Storage& storage);
+
+	// JSON
+	void	SetFromJSON(const ptree& pt);
+	void	Load(const ptree& pt) override { SetFromJSON(pt); }
+	void	AddToJSON(ptree& pt) const;
+	void	Save(ptree& pt) const override { AddToJSON(pt); }
 };
+
+inline void Storage::SetFromJSON(const ptree& pt)
+{
+	// molecules
+	for (auto m : pt.get_child("molecules"))
+	{
+		const auto& model = g_assetManagerPtr->GetModel<MoleculeModel>(m.second.get<string>("model"));
+
+		m_molecules.emplace_back(model, m.second.get<int>("size"));
+	}
+
+	Model::SetFromJSON(pt);
+}
+
+inline void Storage::AddToJSON(ptree& pt) const
+{
+	// molecules
+	{
+		ptree molecules;
+
+		{
+			ptree molecule;
+
+			for (const auto& m : m_molecules)
+			{
+				molecule.put("model", m.first->GetName());
+				molecule.put("size", m.second);
+
+				molecules.push_back(std::make_pair("", molecule));
+			}
+		}
+
+		pt.add_child("molecules", molecules);
+	}
+
+	Model::AddToJSON(pt);
+
+	// type
+	pt.put("type", "Storage");
+}
