233745765de4143264b5f59624b1e72a50e6eaaa
sknjpn <sknjpn@gmail.com>
2019-06-29 16:43:38 +0900

    更新

---
+++ SyLife/Synthesizer.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Module.h"
+#include "Storage.h"
+
+class SynthesizerModel
+	: public ModuleModel
+{
+	Storage	m_material;
+	Storage	m_product;
+
+public:
+	const Storage&	GetMaterial() const { return m_material; }
+	const Storage&	GetProduct() const { return m_product; }
+
+	shared_ptr<PartState>	MakeState() override;
+
+	void	SetFromJSON(const ptree& pt);
+	void	Load(const ptree& pt) override { SetFromJSON(pt); }
+};
+
+class SynthesizerState
+	: public ModuleState
+{
+public:
+	void	Draw(const CellState& cell) const { m_config->m_model->Draw(); }
+	void	Update(CellState& cell) override 
+	{
+
+	}
+};
+
+inline shared_ptr<PartState>	SynthesizerModel::MakeState() { return make_shared<SynthesizerState>(); }
+
+inline void SynthesizerModel::SetFromJSON(const ptree& pt)
+{
+	ModuleModel::SetFromJSON(pt);
+}
