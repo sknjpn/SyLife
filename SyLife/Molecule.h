12159f9681606daf249f1efbfecf614f60bd1be7
sknjpn <sknjpn@gmail.com>
2019-06-29 15:37:43 +0900

    問題の修正

--- SyLife/Molecule.h
+++ SyLife/Molecule.h
@@ -33,3 +33,17 @@ public:
 	
 public:
 };
+
+inline void MoleculeModel::SetFromJSON(const ptree& pt)
+{
+	// mass
+	m_mass = pt.get<double>("mass");
+
+	// color
+	m_color = s3d::Color(pt.get<int>("color.r"), pt.get<int>("color.g"), pt.get<int>("color.b"));
+
+	// radius
+	m_radius = sqrt(m_mass);
+
+	Model::SetFromJSON(pt);
+}
