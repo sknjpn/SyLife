4b5e391ecc6d48769b14f1815ac3e6557d36c32b
sknjpn <sknjpn@gmail.com>
2019-06-30 12:18:02 +0900

    Storageの移植

--- Editor/Storage.h
+++ Editor/Storage.h
@@ -1,32 +1,85 @@
 #pragma once
 
 #include "Model.h"
-#include "AssetManager.h"
 #include "Molecule.h"
+#include "AssetManager.h"
 
 class MoleculeModel;
 
 class Storage
 	: public Model
 {
-public:
 	vector<pair<shared_ptr<MoleculeModel>, unsigned int>>	m_molecules;
 
 public:
-	void	AddMoleculeState(const shared_ptr<MoleculeModel>& model, unsigned int size = 1);
-	int		NumMolecule(const shared_ptr<MoleculeModel>& model) const;
-	int		NumMolecule(const string& name) const;
-	bool	HasMolecule(const Storage& storage);
-	void	PullMolecule(const shared_ptr<MoleculeModel>& model, unsigned int size = 1);
-	void	PullMolecule(const Storage& storage);
+	// operator
+	Storage	operator +(const Storage& s) const { return Storage(*this) += s; }
+	Storage	operator -(const Storage& s) const { return Storage(*this) -= s; }
+	bool operator >=(const Storage& s) const
+	{
+		for (const auto& m : s.m_molecules)
+			if (Num(m.first) < m.second) return false;
+
+		return true;
+	}
+	bool operator <=(const Storage& s) const
+	{
+		for (const auto& m : m_molecules)
+			if (s.Num(m.first) < m.second) return false;
+
+		return true;
+	}
+	Storage& operator +=(const Storage& s) noexcept
+	{
+		for (const auto& m : s.m_molecules)
+			Add(m.first, m.second);
+
+		return *this;
+	}
+	Storage& operator -=(const Storage& s) noexcept
+	{
+		for (const auto& m : s.m_molecules)
+			Pull(m.first, m.second);
+
+		return *this;
+	}
+
+	// molecule
+	void	Add(const shared_ptr<MoleculeModel>& model, unsigned int size = 1)
+	{
+		auto it = find_if(m_molecules.begin(), m_molecules.end(), [&model](const auto& m) { return m.first == model; });
+
+		if (it == m_molecules.end()) m_molecules.emplace_back(model, size);
+		else (*it).second += size;
+	}
+	void	Pull(const shared_ptr<MoleculeModel>& model, unsigned int size = 1)
+	{
+		auto it = find_if(m_molecules.begin(), m_molecules.end(), [&model](const auto& m) { return m.first == model; });
+
+		if (it == m_molecules.end()) throw new exception;
+		else
+		{
+			if (((*it).second -= size) < 0) throw new exception;
+			else if ((*it).second == 0) m_molecules.erase(it);
+		}
+	}
+	unsigned int	Num(const shared_ptr<MoleculeModel>& model) const
+	{
+		auto it = find_if(m_molecules.begin(), m_molecules.end(), [&model](const auto& m) { return m.first == model; });
+
+		if (it == m_molecules.end()) return 0;
+		else return (*it).second;
+	}
+
+	const vector<pair<shared_ptr<MoleculeModel>, unsigned int>>&	GetMolecules() const { return m_molecules; }
+
+	bool	IsEmpty() const { return m_molecules.empty(); }
+
 	void	Clear() { m_molecules.clear(); }
-	void	AddStorage(const Storage& storage);
 
 	// JSON
 	void	SetFromJSON(const ptree& pt);
 	void	Load(const ptree& pt) override { SetFromJSON(pt); }
-	void	AddToJSON(ptree& pt) const;
-	void	Save(ptree& pt) const override { AddToJSON(pt); }
 };
 
 inline void Storage::SetFromJSON(const ptree& pt)
@@ -34,37 +87,12 @@ inline void Storage::SetFromJSON(const ptree& pt)
 	// molecules
 	for (auto m : pt.get_child("molecules"))
 	{
-		const auto& model = g_assetManagerPtr->GetModel<MoleculeModel>(m.second.get<string>("model"));
+		auto name = m.second.get<string>("molecule");
+
+		const auto& model = g_assetManagerPtr->GetModel<MoleculeModel>(name);
 
 		m_molecules.emplace_back(model, m.second.get<int>("size"));
 	}
 
 	Model::SetFromJSON(pt);
-}
-
-inline void Storage::AddToJSON(ptree& pt) const
-{
-	// molecules
-	{
-		ptree molecules;
-
-		{
-			ptree molecule;
-
-			for (const auto& m : m_molecules)
-			{
-				molecule.put("model", m.first->GetName());
-				molecule.put("size", m.second);
-
-				molecules.push_back(std::make_pair("", molecule));
-			}
-		}
-
-		pt.add_child("molecules", molecules);
-	}
-
-	Model::AddToJSON(pt);
-
-	// type
-	pt.put("type", "Storage");
 }
