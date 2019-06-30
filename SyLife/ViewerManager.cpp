e54fe5c30b04b73b2b943bd18aa7789b086f4597
sknjpn <sknjpn@gmail.com>
2019-06-30 20:07:01 +0900

    背景色の描画追加

--- SyLife/ViewerManager.cpp
+++ SyLife/ViewerManager.cpp
@@ -22,6 +22,8 @@ void ViewerManager::Update()
 		auto vp = s3d::ViewportBlock2D(s3d::Rect((*it)->m_drawRect));
 		auto t = s3d::Transformer2D(s3d::Mat3x2::Identity(), s3d::Mat3x2::Translate((*it)->m_drawRect.pos));
 
+		s3d::RectF((*it)->m_drawRect.size).draw((*it)->m_backgroundColor);
+
 		(*it)->Update(mouseOverViewer == *it);
 
 		s3d::RectF((*it)->m_drawRect.size).drawFrame(1.0, 0.0, s3d::ColorF(s3d::Palette::Red, 0.5));
