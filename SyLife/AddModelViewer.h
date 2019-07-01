cf7e19921ac8630fb8f9348bf24326accc8cc1ae
sknjpn <sknjpn@gmail.com>
2019-07-01 23:15:34 +0900

    Viewerの移植

---
+++ SyLife/AddModelViewer.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Viewer.h"
+#include "AssetManager.h"
+
+class AddModelViewer
+	: public Viewer
+{
+	Vec2	m_drawPos;
+public:
+	AddModelViewer()
+	{
+		m_drawRect = RectF(100, 150, 400, 300);
+	}
+
+	template <typename T>
+	void	AddModel()
+	{
+		for (int i = 1;; ++i)
+		{
+			string name = "model_" + to_string(i);
+
+			if (!g_assetManagerPtr->HasModel(name))
+			{
+				auto m  = g_assetManagerPtr->MakeModel<T>();
+
+				m->SetName(name);
+
+				return;
+			}
+		}
+	}
+
+	template <typename T>
+	void	DrawModels(const string& name)
+	{
+		static Font font10(10, Typeface::Bold);
+		auto t = Transformer2D(Mat3x2::Translate(m_drawPos), true);
+		auto f = font10(Unicode::Widen(name));
+
+		f.region().draw(ColorF(1.0, f.region().mouseOver() ? 0.5 : 0.0));
+		f.draw();
+
+		if (f.region().leftClicked()) AddModel<T>();
+
+		m_drawPos.moveBy(0.0, 15.0);
+	}
+
+	void	Update() override;
+};
+
---
+++ SyLife/AssetViewer.cpp
@@ -0,0 +1,43 @@
+#include "AssetViewer.h"
+#include "AssetManager.h"
+#include "AddModelViewer.h"
+#include "Cell.h"
+#include "Molecule.h"
+#include "Part.h"
+#include "Body.h"
+#include "Equipment.h"
+#include "Module.h"
+
+void AssetViewer::Update()
+{
+	m_drawPos = Vec2::Zero();
+
+	DrawModels<CellModel>("CellModel");
+	DrawModels<MoleculeModel>("MoleculeModel");
+	DrawModels<BodyModel>("BodyModel");
+	DrawModels<EquipmentModel>("EquipmentModel");
+	DrawModels<ModuleModel>("ModuleModel");
+
+	// AddNewModel
+	{
+		static Font font(15, Typeface::Bold);
+
+		auto t = Transformer2D(Mat3x2::Translate(m_drawPos), true);
+		auto f = font(U"V‚µ‚¢Model‚Ì’Ç‰Á");
+
+		f.region().draw(ColorF(1.0, f.region().mouseOver() ? 0.5 : 0.0));
+		f.draw();
+
+		if (f.region().leftClicked() && !g_viewerManagerPtr->HasViewer<AddModelViewer>()) g_viewerManagerPtr->MakeViewer<AddModelViewer>();
+
+		m_drawPos.moveBy(0.0, 16.0);
+	}
+
+	// Save
+	if (KeyControl.pressed() && KeyS.down() && m_selectedModel != nullptr)
+	{
+		ptree pt;
+		m_selectedModel->Save(pt);
+		write_json(m_selectedModel->GetFilepath(), pt);
+	}
+}
