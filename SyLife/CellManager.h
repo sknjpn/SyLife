ffc185609f0da887d02b0eac55a0f259e2f87b46
sknjpn <sknjpn@gmail.com>
2019-07-03 19:43:14 +0900

    インデント修正

--- SyLife/CellManager.h
+++ SyLife/CellManager.h
@@ -12,9 +12,9 @@ class CellManager
 public:
 
 	// Get
-	Indexer<CellState>&	GetIndexer() { return m_indexer; }
+	Indexer<CellState>&			GetIndexer() { return m_indexer; }
 	const Indexer<CellState>&	GetIndexer() const { return m_indexer; }
-	vector<shared_ptr<CellState>>&	GetCellStates() { return m_indexer.m_cloud.m_particles; }
+	vector<shared_ptr<CellState>>&			GetCellStates() { return m_indexer.m_cloud.m_particles; }
 	const vector<shared_ptr<CellState>>&	GetCellStates() const { return m_indexer.m_cloud.m_particles; }
 
 	const shared_ptr<CellState>&	AddCellState(const shared_ptr<CellModel>& model);
