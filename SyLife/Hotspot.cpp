684df50d849f6c1fd41877f0bf7accceb48bd4ff
sknjpn <sknjpn@gmail.com>
2019-07-04 18:52:39 +0900

    HotspotModelの追加

---
+++ SyLife/Hotspot.cpp
@@ -0,0 +1,19 @@
+#include "Hotspot.h"
+
+void HotspotState::UpdateHotspot()
+{
+	m_timer -= g_fieldManagerPtr->GetDeltaTime();
+
+	if (m_timer < 0.0)
+	{
+
+	}
+}
+
+void HotspotState::Draw()
+{
+	auto t = Transformer2D(Mat3x2::Translate(GetPosition()));
+
+	for (int s = 100; s > 0; s -= 10)
+		Circle(s).draw(ColorF(Palette::Darkred, 1.0 - s / 100.0));
+}
