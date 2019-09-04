cd89b6e25330ec6107d2838330d784efe9bced8d
SKNJPN <sknjpn@gmail.com>
2019-09-04 22:24:31 +0900

    アセンブリのパーツ選択をGUIに変更

--- SyLife/PartPaletteViewer.h
+++ SyLife/PartPaletteViewer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ModelViewer.h"
+#include "ShapeModel.h"
 
 class PartModel;
 
@@ -22,23 +23,32 @@ public:
 		static Font font13(12, Typeface::Bold);
 		static Font font10(10, Typeface::Bold);
 
-			font13(Unicode::Widen(name)).draw();
-			MoveDrawPos(0, 16);
+		font13(Unicode::Widen(name)).draw();
+		MoveDrawPos(0, 16);
 
-			MoveDrawPos(16, 0);
 		const auto& models = g_assetManagerPtr->GetModels<T>();
 		for (auto it = models.begin(); it != models.end(); ++it)
 		{
-			auto f = font10(Unicode::Widen((*it)->GetName()));
+			Rect(64, 64).stretched(-2).draw(ColorF(1.0, 0.25)).drawFrame(1.0, Palette::White);
 
-			f.region().draw(ColorF(1.0, f.region().mouseOver() ? 0.5 : 0.0));
-			f.draw();
+			MoveDrawPos(4, 4);
+			{
+				auto f = font10(Unicode::Widen((*it)->GetName()));
+				f.region().draw(ColorF(1.0, f.region().mouseOver() ? 0.5 : 0.0));
+				f.draw();
 
-			if (f.region().leftClicked()) m_selectedPart = *it;
+				if (f.region().leftClicked()) m_selectedPart = *it;
 
-			MoveDrawPos(0, 15);
+				// ƒp[ƒc•`‰æ
+				for (const ShapeModel& s : (*it)->GetShapes())
+				{
+					s.Draw(0.5);
+				}
+			}
+			MoveDrawPos(-4, -4);
+
+			MoveDrawPos(0, 64);
 		}
-		MoveDrawPos(-16, 0);
 	}
 
 	void	Update() override;
