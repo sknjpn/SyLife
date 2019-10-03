422011a9ae396503fd1aa9f4574bc8471427a048
SKNJPN <sknjpn@gmail.com>
2019-10-03 19:32:20 +0900

    moleculeStateの削除

--- SyLife/Molecule.h
+++ SyLife/Molecule.h
@@ -60,27 +60,6 @@ public:
 	string	GetFilepath() const override { return "assets/models/molecules/" + GetFilename(); }
 };
 
-class MoleculeState
-	: public Rigidbody
-{
-	shared_ptr<MoleculeModel>	m_model;
-
-public:
-	// Get
-	const shared_ptr<MoleculeModel>&	GetModel() const { return m_model; }
-
-	// Set
-	void	SetModel(const shared_ptr<MoleculeModel>& model)
-	{
-		m_model = model;
-		SetRadius(model->GetRadius());
-		SetMass(model->GetMass());
-	}
-
-	void	UpdateMolecule();
-	void	Draw();
-};
-
 class MoleculeViewer
 	: public ModelViewer
 {
