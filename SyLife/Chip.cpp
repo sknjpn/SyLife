b0d933f289348d41002b8bb1f6ef4976cc409758
SKNJPN <sknjpn@gmail.com>
2019-10-03 21:12:27 +0900

    初期化が出来ていない問題の修正

--- SyLife/Chip.cpp
+++ SyLife/Chip.cpp
@@ -2,7 +2,7 @@
 
 Color Chip::getColor() const
 {
-	double r, g, b = 0.0;
+	double r = 0.0, g = 0.0, b = 0.0;
 	for (const auto& m : m_storage.GetMolecules())
 	{
 		r += m.first->GetColor().r / 255.0 * m.second / m_storage.Num();
@@ -10,5 +10,5 @@ Color Chip::getColor() const
 		b += m.first->GetColor().b / 255.0 * m.second / m_storage.Num();
 	}
 
-	return Color(r, g, b);
+	return ColorF(r, g, b);
 }
