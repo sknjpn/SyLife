00ce3b63ae5f3b9d7c8235e01bf0631bdb62f3ef
SKNJPN <sknjpn@gmail.com>
2019-10-05 16:42:42 +0900

    Materialクラスの作成

---
+++ SyLife/Material.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "Storage.h"
+
+// あるものを生産するのに必要な量
+class Material
+{
+	Storage m_storage;
+	double	m_nutrition;
+
+public:
+	const Storage& getStorage() const { return m_storage; }
+	double	getNutrition() const { return m_nutrition; }
+};
+
