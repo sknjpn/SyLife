9a82bc7d72edad4ad35fac5ada38b611145103a0
SKNJPN <sknjpn@gmail.com>
2019-10-06 16:42:32 +0900

    Storageクラスの改善

---
+++ SyLife/Storage.cpp
@@ -0,0 +1,100 @@
+﻿#include "Storage.h"
+
+bool Storage::operator>=(const Storage& s) const
+{
+	for (const auto& m : s)
+		if (numMolecule(m.first) < m.second) return false;
+
+	return true;
+}
+
+bool Storage::operator<=(const Storage& s) const
+{
+	for (const auto& m : s)
+		if (s.numMolecule(m.first) < m.second) return false;
+
+	return true;
+}
+
+Storage& Storage::operator+=(const Storage& s) noexcept
+{
+	for (const auto& m : s)
+		addMolecule(m.first, m.second);
+
+	return *this;
+}
+
+Storage& Storage::operator-=(const Storage& s) noexcept
+{
+	for (const auto& m : s)
+		pullMolecule(m.first, m.second);
+
+	return *this;
+}
+
+void Storage::addMolecule(const shared_ptr<MoleculeModel>& model, int size)
+{
+	auto it = find_if(begin(), end(), [&model](const auto& m) { return m.first == model; });
+
+	if (it == end()) emplace_back(model, size);
+	else (*it).second += size;
+}
+
+void Storage::pullMolecule(const shared_ptr<MoleculeModel>& model, int size)
+{
+	auto it = find_if(begin(), end(), [&model](const auto& m) { return m.first == model; });
+
+	if (it == end()) throw Error(U"全く存在しないMoleculeの削除を試みました");
+	else
+	{
+		if (((*it).second -= size) < 0) throw Error(U"存在しない量のMoleculeの削除を試みました");
+		else if ((*it).second == 0) erase(it);
+	}
+}
+
+int Storage::numMolecule(const shared_ptr<MoleculeModel>& model) const
+{
+	auto it = find_if(begin(), end(), [&model](const auto& m) { return m.first == model; });
+
+	if (it == end()) return 0;
+	else return (*it).second;
+}
+
+void Storage::load_this(const ptree& pt)
+{
+	// molecules
+	for (auto m : pt.get_child("molecules"))
+	{
+		auto name = m.second.get<string>("name");
+
+		const auto& model = g_assetManagerPtr->GetModel<MoleculeModel>(name);
+
+		emplace_back(model, m.second.get<int>("size"));
+	}
+
+	Model::load_this(pt);
+}
+
+void Storage::save_this(ptree& pt) const
+{
+	// molecules
+	{
+		ptree molecules;
+
+		for (const auto& m : *this)
+		{
+			ptree part;
+			part.put<string>("name", m.first->GetName());
+			part.put<int>("size", m.second);
+
+			molecules.push_back(std::make_pair("", part));
+		}
+
+		pt.add_child("molecules", molecules);
+	}
+
+	Model::save_this(pt);
+
+	// type
+	pt.put("type", "Storage");
+}
--- SyLife/Storage.h
+++ SyLife/Storage.h
@@ -7,124 +7,29 @@ class MoleculeModel;
 
 class Storage
 	: public Model
-{
-	Array<pair<shared_ptr<MoleculeModel>, double>>	m_molecules;
+	, private Array<pair<shared_ptr<MoleculeModel>, int>>
+{	
+	double m_nutrition;
 
 public:
+
 	// operator
-	Storage	operator +(const Storage& s) const { return Storage(*this) += s; }
-	Storage	operator -(const Storage& s) const { return Storage(*this) -= s; }
-	bool operator >=(const Storage& s) const
-	{
-		for (const auto& m : s.m_molecules)
-			if (Num(m.first) < m.second) return false;
-
-		return true;
-	}	
-	bool operator <=(const Storage& s) const
-	{
-		for (const auto& m : m_molecules)
-			if (s.Num(m.first) < m.second) return false;
-
-		return true;
-	}
-	Storage& operator +=(const Storage& s) noexcept
-	{
-		for (const auto& m : s.m_molecules)
-			Add(m.first, m.second);
-
-		return *this;
-	}
-	Storage& operator -=(const Storage& s) noexcept
-	{
-		for (const auto& m : s.m_molecules)
-			Pull(m.first, m.second);
-
-		return *this;
-	}
+	Storage	operator+(const Storage& s) const { return Storage(*this) += s; }
+	Storage	operator-(const Storage& s) const { return Storage(*this) -= s; }
+	bool operator>=(const Storage& s) const;
+	bool operator<=(const Storage& s) const;
+	Storage& operator+=(const Storage& s) noexcept;
+	Storage& operator-=(const Storage& s) noexcept;
 
 	// molecule
-	void	Add(const shared_ptr<MoleculeModel>& model, double size)
-	{
-		auto it = find_if(m_molecules.begin(), m_molecules.end(), [&model](const auto& m) { return m.first == model; });
-
-		if (it == m_molecules.end()) m_molecules.emplace_back(model, size);
-		else (*it).second += size;
-	}
-
-	void	Pull(const shared_ptr<MoleculeModel>& model, double size)
-	{
-		auto it = find_if(m_molecules.begin(), m_molecules.end(), [&model](const auto& m) { return m.first == model; });
-
-		if (it == m_molecules.end()) throw new exception;
-		else
-		{
-			if (((*it).second -= size) < 0.0) throw new exception;
-			else if ((*it).second == 0.0) m_molecules.erase(it);
-		}
-	}
-
-	double	Num(const shared_ptr<MoleculeModel>& model) const
-	{
-		auto it = find_if(m_molecules.begin(), m_molecules.end(), [&model](const auto& m) { return m.first == model; });
-
-		if (it == m_molecules.end()) return 0.0;
-		else return (*it).second;
-	}
-
-	double	Num() const
-	{
-		double sum = 0.0;
-		for (const auto& m : m_molecules)
-			sum += m.second;
-
-		return sum;
-	}
-
-	const Array<pair<shared_ptr<MoleculeModel>, double>>&	GetMolecules() const { return m_molecules; }
-
-	bool	isEmpty() const { return m_molecules.empty(); }
-
-	void	clear() { m_molecules.clear(); }
+	Array<pair<shared_ptr<MoleculeModel>, int>>	getMoleculeList() const { return *this; }
+	void	addMolecule(const shared_ptr<MoleculeModel>& model, int size);
+	void	pullMolecule(const shared_ptr<MoleculeModel>& model, int size);
+	int		numMolecule(const shared_ptr<MoleculeModel>& model) const;
 
 	// JSON
-	void	load_this(const ptree& pt)
-	{
-		// molecules
-		for (auto m : pt.get_child("molecules"))
-		{
-			auto name = m.second.get<string>("molecule");
-
-			const auto& model = g_assetManagerPtr->GetModel<MoleculeModel>(name);
-
-			m_molecules.emplace_back(model, m.second.get<double>("size"));
-		}
-
-		Model::load_this(pt);
-	}
+	void	load_this(const ptree& pt);
 	void	load(const ptree& pt) override { load_this(pt); }
-	void	save_this(ptree& pt) const 
-	{
-		// molecules
-		{
-			ptree molecules;
-
-			for (const auto& m : m_molecules)
-			{
-				ptree part;
-				part.put<string>("molecule", m.first->GetName());
-				part.put<int>("size", m.second);
-
-				molecules.push_back(std::make_pair("", part));
-			}
-
-			pt.add_child("molecules", molecules);
-		}
-
-		Model::save_this(pt);
-
-		// type
-		pt.put("type", "Storage");
-	}
+	void	save_this(ptree& pt) const;
 	void	save(ptree& pt) const override { save_this(pt); }
 };
