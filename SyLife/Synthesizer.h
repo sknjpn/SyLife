d4ae9df6019b42276f4d21d84a793b266d4479e5
SKNJPN <sknjpn@gmail.com>
2019-10-06 16:58:32 +0900

    数値入力の問題修正

--- SyLife/Synthesizer.h
+++ SyLife/Synthesizer.h
@@ -61,7 +61,7 @@ public:
 			m_timer = 0.0;
 
 			cell.m_storage -= model->GetImport();
-			cell.m_storage.addMolecule(model->GetExport(), 1.0);
+			cell.m_storage.addMolecule(model->GetExport(), 1);
 		}
 	}
 };
