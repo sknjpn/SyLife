b1c74374e3e83855f242b906d4cf71d83d2935da
SKNJPN <sknjpn@gmail.com>
2019-10-06 18:03:03 +0900

    includeの修正

---
+++ SyLife/Chloroplast.cpp
@@ -0,0 +1,22 @@
+﻿#include "Chloroplast.h"
+
+#include "ViewerManager.h"
+
+void ChloroplastState::Update(CellState& cell)
+{
+}
+
+void ChloroplastState::MakeNutrition()
+{
+}
+
+void ChloroplastModel::MakeViewer()
+{
+	g_viewerManagerPtr->MakeViewer<ChloroplastViewer>()->SetModel(shared_from_this());
+	g_viewerManagerPtr->MakeViewer<PartShapeViewer>()->SetModel(shared_from_this());
+}
+
+shared_ptr<PartState> ChloroplastModel::MakeState()
+{
+	return make_shared<ChloroplastState>();
+}
--- SyLife/Chloroplast.h
+++ SyLife/Chloroplast.h
@@ -9,7 +9,7 @@ public:
 	void		MakeViewer() override;
 	shared_ptr<PartState>	MakeState() override;
 
-	void	load_this(const ptree& pt);
+	void	load_this(const ptree& pt) { ModuleModel::load_this(pt); }
 	void	load(const ptree& pt) override { load_this(pt); }
 	void	save_this(ptree& pt) const
 	{
@@ -26,7 +26,7 @@ class ChloroplastState
 {
 public:
 	void	Draw(const CellState& cell) const { GetPartConfig()->GetModel()->Draw(); }
-	void	Update(CellState& cell) override {}
+	void	Update(CellState& cell) override;
 
 	void	MakeNutrition();
 };
@@ -49,19 +49,3 @@ public:
 	}
 	void Update() override { Update_this(); }
 };
-
-inline void		ChloroplastModel::MakeViewer()
-{
-	g_viewerManagerPtr->MakeViewer<ChloroplastViewer>()->SetModel(shared_from_this());
-	g_viewerManagerPtr->MakeViewer<PartShapeViewer>()->SetModel(shared_from_this());
-}
-inline shared_ptr<PartState>	ChloroplastModel::MakeState() { return make_shared<ChloroplastState>(); }
-
-inline void ChloroplastModel::load_this(const ptree& pt)
-{
-	ModuleModel::load_this(pt);
-}
-
-inline void ChloroplastState::MakeNutrition()
-{
-}
