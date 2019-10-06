3a332b2d36235ab182cce82f172870301e4c2da6
SKNJPN <sknjpn@gmail.com>
2019-10-06 17:56:14 +0900

    関数をsrcに移動

--- SyLife/Molecule.cpp
+++ SyLife/Molecule.cpp
@@ -6,3 +6,38 @@ inline void MoleculeModel::MakeViewer()
 {
 	g_viewerManagerPtr->MakeViewer<MoleculeViewer>()->SetModel(shared_from_this());
 }
+
+void MoleculeModel::load_this(const ptree& pt)
+{
+	// mass
+	m_mass = pt.get<double>("mass");
+
+	// color
+	m_color = Color(pt.get<int>("color.r"), pt.get<int>("color.g"), pt.get<int>("color.b"));
+
+	// radius
+	m_radius = sqrt(m_mass);
+
+	Model::load_this(pt);
+}
+
+void MoleculeModel::save_this(ptree& pt) const
+{
+	// mass
+	pt.put<double>("mass", m_mass);
+
+	// color
+	{
+		ptree color;
+
+		color.put("r", m_color.r);
+		color.put("g", m_color.g);
+		color.put("b", m_color.b);
+
+		pt.add_child("color", color);
+	}
+
+	Model::save_this(pt);
+
+	pt.put("type", "MoleculeModel");
+}
--- SyLife/Molecule.h
+++ SyLife/Molecule.h
@@ -22,40 +22,9 @@ public:
 	const Color&	GetColor() const { return m_color; }
 
 	// JSON
-	void	load_this(const ptree& pt)
-	{
-		// mass
-		m_mass = pt.get<double>("mass");
-
-		// color
-		m_color = Color(pt.get<int>("color.r"), pt.get<int>("color.g"), pt.get<int>("color.b"));
-
-		// radius
-		m_radius = sqrt(m_mass);
-
-		Model::load_this(pt);
-	}
+	void	load_this(const ptree& pt);
 	void	load(const ptree& pt) override { load_this(pt); }
-	void	save_this(ptree& pt) const
-	{
-		// mass
-		pt.put<double>("mass", m_mass);
-
-		// color
-		{
-			ptree color;
-
-			color.put("r", m_color.r);
-			color.put("g", m_color.g);
-			color.put("b", m_color.b);
-
-			pt.add_child("color", color);
-		}
-
-		Model::save_this(pt);
-
-		pt.put("type", "MoleculeModel");
-	}
+	void	save_this(ptree& pt) const;
 	void	save(ptree& pt) const override { save_this(pt); }
 	string	GetFilepath() const override { return "assets/models/molecules/" + GetFilename(); }
 };
