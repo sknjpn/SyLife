fefefef502a9bc05aa341c14c5a0fb071f4990a7
SKNJPN <sknjpn@gmail.com>
2019-10-06 17:46:15 +0900

    stdafx.hからManagerのincludeを削除

--- SyLife/stdafx.h
+++ SyLife/stdafx.h
@@ -5,15 +5,3 @@
 #include <Siv3D.hpp> // OpenSiv3D v0.4.0
 
 using namespace std;
-
-// manager
-#include "AssetManager.h"
-#include "CellManager.h"
-#include "EggManager.h"
-#include "FieldManager.h"
-#include "SystemManager.h"
-#include "ViewerManager.h"
-#include "WaveManager.h"
-#include "TerrainManager.h"
-#include "HotspotManager.h"
-#include "ChipManager.h"
