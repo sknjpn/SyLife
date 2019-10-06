6f470eecc7cc12971acb8e1239a6acf1c6fe1951
SKNJPN <sknjpn@gmail.com>
2019-10-06 18:04:14 +0900

    includeの修正

---
+++ SyLife/Equipment.cpp
@@ -0,0 +1,13 @@
+﻿#include "Equipment.h"
+#include "ViewerManager.h"
+
+void EquipmentModel::MakeViewer()
+{
+	g_viewerManagerPtr->MakeViewer<EquipmentViewer>()->SetModel(shared_from_this());
+	g_viewerManagerPtr->MakeViewer<PartShapeViewer>()->SetModel(shared_from_this());
+}
+
+shared_ptr<PartState> EquipmentModel::MakeState()
+{
+	return make_shared<EquipmentState>();
+}
--- SyLife/Equipment.h
+++ SyLife/Equipment.h
@@ -9,7 +9,7 @@ public:
 	void MakeViewer() override;
 	shared_ptr<PartState>	MakeState() override;
 
-	void	load_this(const ptree& pt);
+	void	load_this(const ptree& pt) { PartModel::load_this(pt); }
 	void	load(const ptree& pt) override { load_this(pt); }
 	void	save_this(ptree& pt) const
 	{
@@ -48,15 +48,3 @@ public:
 	}
 	void Update() override { Update_this(); }
 };
-
-inline void		EquipmentModel::MakeViewer()
-{
-	g_viewerManagerPtr->MakeViewer<EquipmentViewer>()->SetModel(shared_from_this());
-	g_viewerManagerPtr->MakeViewer<PartShapeViewer>()->SetModel(shared_from_this());
-}
-inline shared_ptr<PartState>	EquipmentModel::MakeState() { return make_shared<EquipmentState>(); }
-
-inline void EquipmentModel::load_this(const ptree& pt)
-{
-	PartModel::load_this(pt);
-}
