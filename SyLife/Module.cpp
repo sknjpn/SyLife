3ca38cfcff3491867ad836ee90275d9a10e41069
SKNJPN <sknjpn@gmail.com>
2019-10-06 18:06:11 +0900

    includeの修正

---
+++ SyLife/Module.cpp
@@ -0,0 +1,14 @@
+﻿#include "Module.h"
+
+#include "ViewerManager.h"
+
+void ModuleModel::MakeViewer()
+{
+	g_viewerManagerPtr->MakeViewer<ModuleViewer>()->SetModel(shared_from_this());
+	g_viewerManagerPtr->MakeViewer<PartShapeViewer>()->SetModel(shared_from_this());
+}
+
+shared_ptr<PartState> ModuleModel::MakeState()
+{
+	return make_shared<ModuleState>();
+}
--- SyLife/Module.h
+++ SyLife/Module.h
@@ -9,7 +9,7 @@ public:
 	void MakeViewer() override;
 	shared_ptr<PartState>	MakeState() override;
 
-	void	load_this(const ptree& pt);
+	void	load_this(const ptree& pt) { PartModel::load_this(pt); }
 	void	load(const ptree& pt) override { load_this(pt); }
 	void	save_this(ptree& pt) const
 	{
@@ -47,16 +47,3 @@ public:
 	}
 	void Update() override { Update_this(); }
 };
-
-inline void		ModuleModel::MakeViewer()
-{
-	g_viewerManagerPtr->MakeViewer<ModuleViewer>()->SetModel(shared_from_this());
-	g_viewerManagerPtr->MakeViewer<PartShapeViewer>()->SetModel(shared_from_this());
-}
-
-inline shared_ptr<PartState>	ModuleModel::MakeState() { return make_shared<ModuleState>(); }
-
-inline void ModuleModel::load_this(const ptree& pt)
-{
-	PartModel::load_this(pt);
-}
