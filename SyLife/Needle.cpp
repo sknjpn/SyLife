59bf0d678ec5cc5dc57290b650c036e3bfb204bf
SKNJPN <sknjpn@gmail.com>
2019-10-06 18:07:54 +0900

    includeの修正

---
+++ SyLife/Needle.cpp
@@ -0,0 +1,47 @@
+﻿#include "Needle.h"
+
+#include "SystemManager.h"
+#include "ViewerManager.h"
+#include "CellManager.h"
+
+void NeedleModel::MakeViewer()
+{
+	g_viewerManagerPtr->MakeViewer<NeedleViewer>()->SetModel(shared_from_this());
+	g_viewerManagerPtr->MakeViewer<PartShapeViewer>()->SetModel(shared_from_this());
+}
+
+shared_ptr<PartState> NeedleModel::MakeState()
+{
+	return make_shared<NeedleState>();
+}
+
+void NeedleState::Draw(const CellState& cell) const
+{
+	auto t = Transformer2D(Mat3x2::Scale(1.0, max(m_heat - 4.0, 0.0) * 1.0 + 1.0));
+
+	GetPartConfig()->GetModel()->Draw(max(m_heat - 4.0, 0.0) * 0.9 + 0.1);
+}
+
+void NeedleState::Update(CellState& cell)
+{
+	m_heat -= g_systemManagerPtr->GetDeltaTime();
+	if (m_heat < 0)
+	{
+		m_heat = 5.0;
+
+		auto p = cell.GetWorldPosition(GetPartConfig()->GetPosition() + Vec2::Up() * 50.0);
+
+
+		for (auto i : g_cellManagerPtr->GetCellStateKDTree().knnSearch(1, p))
+		{
+			auto& t = g_cellManagerPtr->GetCellStates()[i];
+
+			if (!t->IsDestroyed() && t->GetRadius() > (t->GetPosition() - p).length() && t->m_model != cell.m_model)
+			{
+				t->Destroy();
+				cell.m_storage += t->m_storage;
+				cell.m_storage += t->m_model->GetMaterial();
+			}
+		}
+	}
+}
--- SyLife/Needle.h
+++ SyLife/Needle.h
@@ -10,7 +10,7 @@ public:
 	void	MakeViewer() override;
 	shared_ptr<PartState>	MakeState() override;
 
-	void	load_this(const ptree& pt);
+	void	load_this(const ptree& pt) { EquipmentModel::load_this(pt); }
 	void	load(const ptree& pt) override { load_this(pt); }
 	void	save_this(ptree& pt) const
 	{
@@ -28,35 +28,8 @@ class NeedleState
 	double	m_heat = 0.0;
 
 public:
-	void	Draw(const CellState& cell) const
-	{
-		auto t = Transformer2D(Mat3x2::Scale(1.0, max(m_heat - 4.0, 0.0) * 1.0 + 1.0));
-
-		GetPartConfig()->GetModel()->Draw(max(m_heat - 4.0, 0.0) * 0.9 + 0.1);
-	}
-	void	Update(CellState& cell) override
-	{
-		m_heat -= g_systemManagerPtr->GetDeltaTime();
-		if (m_heat < 0)
-		{
-			m_heat = 5.0;
-
-			auto p = cell.GetWorldPosition(GetPartConfig()->GetPosition() + Vec2::Up() * 50.0);
-
-
-			for (auto i : g_cellManagerPtr->GetCellStateKDTree().knnSearch(1, p))
-			{
-				auto& t = g_cellManagerPtr->GetCellStates()[i];
-
-				if (!t->IsDestroyed() && t->GetRadius() > (t->GetPosition() - p).length() && t->m_model != cell.m_model)
-				{
-					t->Destroy();
-					cell.m_storage += t->m_storage;
-					cell.m_storage += t->m_model->GetMaterial();
-				}
-			}
-		}
-	}
+	void	Draw(const CellState& cell) const;
+	void	Update(CellState& cell) override;
 };
 
 class NeedleViewer
@@ -77,15 +50,3 @@ public:
 	}
 	void Update() override { Update_this(); }
 };
-
-inline void		NeedleModel::MakeViewer()
-{
-	g_viewerManagerPtr->MakeViewer<NeedleViewer>()->SetModel(shared_from_this());
-	g_viewerManagerPtr->MakeViewer<PartShapeViewer>()->SetModel(shared_from_this());
-}
-inline shared_ptr<PartState>	NeedleModel::MakeState() { return make_shared<NeedleState>(); }
-
-inline void NeedleModel::load_this(const ptree& pt)
-{
-	EquipmentModel::load_this(pt);
-}
