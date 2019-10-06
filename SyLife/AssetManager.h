91388706e4bb6741b3a97bbc85c63c9455e5e47f
SKNJPN <sknjpn@gmail.com>
2019-10-06 18:01:23 +0900

    inline関数の削除

--- SyLife/AssetManager.h
+++ SyLife/AssetManager.h
@@ -24,32 +24,26 @@ public:
 	bool	HasModel(const string& name) const { return GetModel(name) != nullptr; }
 
 	template <typename T>
-	shared_ptr<T>		GetModel(const string& name) const;
+	shared_ptr<T>		GetModel(const string& name) const
+	{
+		for (auto it = m_models.begin(); it != m_models.end(); ++it)
+			if ((*it)->GetName() == name && dynamic_pointer_cast<T>(*it) != nullptr) return dynamic_pointer_cast<T>(*it);
+
+		throw new exception;
+	}
 
 	shared_ptr<Model>	GetModel(const string& name) const;
 
 	template <typename T>
-	Array<shared_ptr<T>>	GetModels() const;
+	Array<shared_ptr<T>>	GetModels() const
+	{
+		Array<shared_ptr<T>> tModels;
+
+		for (auto it = m_models.begin(); it != m_models.end(); ++it)
+			if (dynamic_pointer_cast<T>(*it) != nullptr) tModels.emplace_back(dynamic_pointer_cast<T>(*it));
+
+		return tModels;
+	}
 };
 
 extern unique_ptr<AssetManager>	g_assetManagerPtr;
-
-template <typename T>
-inline shared_ptr<T> AssetManager::GetModel(const string& name) const
-{
-	for (auto it = m_models.begin(); it != m_models.end(); ++it)
-		if ((*it)->GetName() == name && dynamic_pointer_cast<T>(*it) != nullptr) return dynamic_pointer_cast<T>(*it);
-
-	throw new exception;
-}
-
-template <typename T>
-inline Array<shared_ptr<T>> AssetManager::GetModels() const
-{
-	Array<shared_ptr<T>> tModels;
-
-	for (auto it = m_models.begin(); it != m_models.end(); ++it)
-		if (dynamic_pointer_cast<T>(*it) != nullptr) tModels.emplace_back(dynamic_pointer_cast<T>(*it));
-
-	return tModels;
-}
