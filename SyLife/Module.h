346dd21f51bb52c5bdc10139f3314af5493e3b00
SKNJPN <sknjpn@gmail.com>
2019-10-07 21:36:42 +0900

    ModuleEditor実装

--- SyLife/Module.h
+++ SyLife/Module.h
@@ -28,22 +28,3 @@ public:
 	void	Draw(const CellState& cell) const { GetPartConfig()->getModel()->Draw(); }
 	void	Update(CellState& cell) override {}
 };
-
-class ModuleViewer
-	: public PartEditor
-{
-public:
-	// Reload
-	void reloadProperties_this()
-	{
-		PartEditor::reloadProperties_this();
-	}
-	void reloadProperties() override { reloadProperties_this(); }
-
-	// Update
-	void Update_this()
-	{
-		PartEditor::Update_this();
-	}
-	void Update() override { Update_this(); }
-};
--- SyLife/ModuleEditor.h
+++ SyLife/ModuleEditor.h
@@ -1,7 +1,22 @@
 ﻿#pragma once
+
 #include "PartEditor.h"
+
 class ModuleEditor :
 	public PartEditor
 {
-};
+public:
+	// Reload
+	void reloadProperties_this()
+	{
+		PartEditor::reloadProperties_this();
+	}
+	void reloadProperties() override { reloadProperties_this(); }
 
+	// Update
+	void Update_this()
+	{
+		PartEditor::Update_this();
+	}
+	void Update() override { Update_this(); }
+};
