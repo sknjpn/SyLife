458f3e5879429e45aea27abc50bf255625dc025d
SKNJPN <sknjpn@gmail.com>
2019-10-07 21:47:08 +0900

    PartStateの分離

--- SyLife/PartModel.h
+++ SyLife/PartModel.h
@@ -45,20 +45,3 @@ public:
 	void	save(ptree& pt) const override { save_this(pt); }
 	string	getFilepath() const override { return "assets/models/parts/" + getFilename(); }
 };
-
-class PartState
-{
-	shared_ptr<PartConfig>	m_partConfig;
-
-public:
-	virtual ~PartState() {}
-
-	// Get
-	const shared_ptr<PartConfig>& GetPartConfig() const { return m_partConfig; }
-
-	// Set
-	void	SetPartConfig(const shared_ptr<PartConfig>& partConfig) { m_partConfig = partConfig; }
-
-	virtual void	Draw(const CellState& cell) const {}
-	virtual void	Update(CellState& cell) {}
-};
---
+++ SyLife/PartState.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+class PartConfig;
+class CellState;
+
+class PartState
+{
+	shared_ptr<PartConfig>	m_partConfig;
+
+public:
+	virtual ~PartState() {}
+
+	// Get
+	const shared_ptr<PartConfig>& GetPartConfig() const { return m_partConfig; }
+
+	// Set
+	void	SetPartConfig(const shared_ptr<PartConfig>& partConfig) { m_partConfig = partConfig; }
+
+	virtual void	Draw(const CellState& cell) const {}
+	virtual void	Update(CellState& cell) {}
+};
