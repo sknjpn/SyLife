bd0ac0531e9e386404feae227eb5df1619a18d05
SKNJPN <sknjpn@gmail.com>
2019-10-07 21:34:47 +0900

    不要な処理の削除

--- SyLife/AssetManager_MakeModel.cpp
+++ SyLife/AssetManager_MakeModel.cpp
@@ -5,13 +5,9 @@
 #include "Body.h"
 #include "Equipment.h"
 #include "Module.h"
-#include "Eye.h"
-#include "Chloroplast.h"
 #include "Synthesizer.h"
 #include "Wing.h"
 #include "Needle.h"
-#include "Hotspot.h"
-#include "Terrain.h"
 
 shared_ptr<Model> AssetManager::makeModel(const string& name, const string& type)
 {
@@ -22,14 +18,9 @@ shared_ptr<Model> AssetManager::makeModel(const string& name, const string& type
 	if (type == "EquipmentModel")	return makeModel<EquipmentModel>(name);
 	if (type == "ModuleModel")		return makeModel<ModuleModel>(name);
 
-	if (type == "EyeModel")			return makeModel<EyeModel>(name);
-	if (type == "ChloroplastModel")	return makeModel<ChloroplastModel>(name);
 	if (type == "SynthesizerModel")	return makeModel<SynthesizerModel>(name);
 	if (type == "WingModel")		return makeModel<WingModel>(name);
 	if (type == "NeedleModel")		return makeModel<NeedleModel>(name);
-
-	if (type == "HotspotModel")		return makeModel<HotspotModel>(name);
-	if (type == "TerrainModel")		return makeModel<TerrainModel>(name);
 
 	return nullptr;
 }
