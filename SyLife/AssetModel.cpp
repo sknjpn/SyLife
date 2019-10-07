5032a99cc2d822808837336e01796ad8feaff38c
SKNJPN <sknjpn@gmail.com>
2019-10-07 20:54:25 +0900

    AssetModelの関数をsrcに移動

--- SyLife/AssetModel.cpp
+++ SyLife/AssetModel.cpp
@@ -1,2 +1,50 @@
-﻿#include "stdafx.h"
-#include "AssetModel.h"
+﻿#include "AssetModel.h"
+
+void AssetModel::SetName(const string& name)
+{
+	// 変化がなければ何もしない
+	if (name == m_name) return;
+
+	// ファイルの削除
+	{
+		const boost::filesystem::path path(GetFilepath());
+
+		boost::filesystem::remove(path);
+	}
+
+	// nameのセット
+	m_name = name;
+
+	// 新規ファイルの作成
+	{
+		ptree pt;
+
+		save(pt);
+
+		write_json(GetFilepath(), pt);
+	}
+}
+
+string AssetModel::GetFilename() const
+{
+	auto filename = m_name + ".json";
+	auto f = [](unsigned char c) { return char(c == 0x20 ? 0x5f : tolower(c)); };
+
+	transform(filename.begin(), filename.end(), filename.begin(), f);
+
+	return filename;
+}
+
+void AssetModel::load_this(const ptree& pt)
+{
+	m_name = pt.get<string>("name");
+}
+
+void AssetModel::save_this(ptree& pt) const
+{
+	// name
+	pt.put("name", m_name);
+
+	// type
+	pt.put("type", "Model");
+}
--- SyLife/AssetModel.h
+++ SyLife/AssetModel.h
@@ -16,61 +16,18 @@ public:
 	virtual void	MakeViewer() {}
 
 	// Set
-	void	SetName(const string& name)
-	{
-		// 変化がなければ何もしない
-		if (name == m_name) return;
-
-		// ファイルの削除
-		{
-			const boost::filesystem::path path(GetFilepath());
-
-			boost::filesystem::remove(path);
-		}
-
-		// nameのセット
-		m_name = name;
-
-		// 新規ファイルの作成
-		{
-			ptree pt;
-
-			save(pt);
-
-			write_json(GetFilepath(), pt);
-		}
-	}
+	void	SetName(const string& name);
 
 	// Get
 	const string& GetName() const { return m_name; }
 
 	// Path
-	virtual string	GetFilename() const
-	{
-		auto filename = m_name + ".json";
-		auto f = [](unsigned char c) { return char(c == 0x20 ? 0x5f : tolower(c)); };
-
-		transform(filename.begin(), filename.end(), filename.begin(), f);
-
-		return filename;
-	}
-	virtual string	GetFilepath() const
-	{
-		return "assets/model/" + GetFilename();
-	}
+	virtual string	GetFilename() const;
+	virtual string	GetFilepath() const { return "assets/model/" + GetFilename(); }
 
 	// JSON
-	void	load_this(const ptree& pt)
-	{
-		m_name = pt.get<string>("name");
-	}
+	void	load_this(const ptree& pt);
 	virtual void	load(const ptree& pt) { load_this(pt); }
-	void	save_this(ptree& pt) const {
-		// name
-		pt.put("name", m_name);
-
-		// type
-		pt.put("type", "Model");
-	}
+	void	save_this(ptree& pt) const;
 	virtual void	save(ptree& pt) const { save_this(pt); }
 };
