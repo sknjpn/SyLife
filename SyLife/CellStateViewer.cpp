480ee61ee4aff4b27f585d86c26397de35d95b62
SKNJPN <sknjpn@gmail.com>
2019-10-08 13:21:51 +0900

    update

---
+++ SyLife/CellStateViewer.cpp
@@ -0,0 +1,71 @@
+﻿#include "CellStateViewer.h"
+
+#include "CellAsset.h"
+#include "CellState.h"
+
+CellStateViewer::CellStateViewer()
+	: m_cellState(nullptr)
+{
+	SetDrawRect(Scene::Width() - 300, 450, 200, 400);
+
+}
+
+void CellStateViewer::update()
+{
+	Rect(GetDrawRect().size.asPoint()).draw(Color(11, 22, 33, 192));
+
+	if (!m_cellState) return;
+	if (m_cellState->IsDestroyed())
+	{
+		m_cellState = nullptr;
+
+		return;
+	}
+
+	const auto& model = m_cellState->m_model;
+
+	{
+		auto t1 = Transformer2D(Mat3x2::Translate(100, 50));
+		auto t2 = Transformer2D(Mat3x2::Scale(50.0 / m_cellState->m_model->getRadius()));
+
+		m_cellState->m_model->draw(1.0);
+	}
+
+	Vec2 pos(0, 100);
+	static Font font(13, Typeface::Bold);
+
+	{
+		{
+			auto t = Transformer2D(Mat3x2::Translate(pos));
+			font(U"Material").draw();
+			pos.moveBy(0, 16);
+		}
+
+		pos.moveBy(16.0, 0.0);
+		for (const auto& m : model->getMaterial().getMoleculeList())
+		{
+			auto t = Transformer2D(Mat3x2::Translate(pos));
+			font(Unicode::Widen(m.first->getName()) + U":" + ToString(m.second) + U"U").draw();
+			pos.moveBy(0, 15);
+		}
+		pos.moveBy(0, 32);
+	}
+
+	{
+		pos.moveBy(-16.0, 0.0);
+		{
+			auto t = Transformer2D(Mat3x2::Translate(pos));
+			font(U"Storage").draw();
+			pos.moveBy(0, 16);
+		}
+
+		pos.moveBy(16.0, 0.0);
+		for (const auto& m : m_cellState->m_storage.getMoleculeList())
+		{
+			auto t = Transformer2D(Mat3x2::Translate(pos));
+			font(Unicode::Widen(m.first->getName()) + U":" + ToString(m.second) + U"U").draw();
+			pos.moveBy(0, 15);
+		}
+		pos.moveBy(-16.0, 0.0);
+	}
+}
--- SyLife/CellStateViewer.h
+++ SyLife/CellStateViewer.h
@@ -1,8 +1,8 @@
 ﻿#pragma once
 
 #include "Viewer.h"
-#include "Cell.h"
-#include "Molecule.h"
+
+class CellState;
 
 class CellStateViewer
 	: public Viewer
@@ -11,72 +11,8 @@ public:
 	shared_ptr<CellState>	m_cellState;
 
 public:
-	CellStateViewer()
-		: m_cellState(nullptr)
-	{
-		SetDrawRect(Scene::Width() - 300, 450, 200, 400);
+	CellStateViewer();
 
-	}
-
-	void	update() override
-	{
-		Rect(GetDrawRect().size.asPoint()).draw(Color(11, 22, 33, 192));
-
-		if (!m_cellState) return;
-		if (m_cellState->IsDestroyed())
-		{
-			m_cellState = nullptr;
-
-			return;
-		}
-
-		const auto& model = m_cellState->m_model;
-
-		{
-			auto t1 = Transformer2D(Mat3x2::Translate(100, 50));
-			auto t2 = Transformer2D(Mat3x2::Scale(50.0 / m_cellState->m_model->getRadius()));
-
-			m_cellState->m_model->draw(1.0);
-		}
-
-		Vec2 pos(0, 100);
-		static Font font(13, Typeface::Bold);
-
-		{
-			{
-				auto t = Transformer2D(Mat3x2::Translate(pos));
-				font(U"Material").draw();
-				pos.moveBy(0, 16);
-			}
-
-			pos.moveBy(16.0, 0.0);
-			for (const auto& m : model->getMaterial().getMoleculeList())
-			{
-				auto t = Transformer2D(Mat3x2::Translate(pos));
-				font(Unicode::Widen(m.first->getName()) + U":" + ToString(m.second) + U"U").draw();
-				pos.moveBy(0, 15);
-			}
-			pos.moveBy(0, 32);
-		}
-
-		{
-			pos.moveBy(-16.0, 0.0);
-			{
-				auto t = Transformer2D(Mat3x2::Translate(pos));
-				font(U"Storage").draw();
-				pos.moveBy(0, 16);
-			}
-
-			pos.moveBy(16.0, 0.0);
-			for (const auto& m : m_cellState->m_storage.getMoleculeList())
-			{
-				auto t = Transformer2D(Mat3x2::Translate(pos));
-				font(Unicode::Widen(m.first->getName()) + U":" + ToString(m.second) + U"U").draw();
-				pos.moveBy(0, 15);
-			}
-			pos.moveBy(-16.0, 0.0);
-		}
-	}
-
+	void	update() override;
 };
 
