241591af0c2ce7540761274394ca2fb9361c2b2f
SKNJPN <sknjpn@gmail.com>
2019-10-08 13:35:13 +0900

    update

--- SyLife/MoleculeEditor.cpp
+++ SyLife/MoleculeEditor.cpp
@@ -1,2 +1,19 @@
 ﻿#include "stdafx.h"
 #include "MoleculeEditor.h"
+
+void MoleculeEditor::reloadProperties_this()
+{
+	ModelEditor::reloadProperties_this();
+
+	m_textEditState_mass.text = ToString(getModel<MoleculeAsset>()->m_mass);
+}
+
+void MoleculeEditor::Update_this()
+{
+	ModelEditor::Update_this();
+
+	// mass
+	SimpleGUI::TextBox(m_textEditState_mass, Vec2(8, 8));
+	getModel<MoleculeAsset>()->m_mass = Parse<double>(m_textEditState_mass.text);
+	MoveDrawPos(0, 48);
+}
--- SyLife/MoleculeEditor.h
+++ SyLife/MoleculeEditor.h
@@ -1,30 +1,18 @@
 ﻿#pragma once
 
+#include "AssetEditor.h"
+
 class MoleculeEditor
-	: public ModelEditor
+	: public AssetEditor
 {
 	TextEditState		m_textEditState_mass;
 
 public:
 	// Reload
-	void reloadProperties_this()
-	{
-		ModelEditor::reloadProperties_this();
-
-		m_textEditState_mass.text = ToString(getModel<MoleculeAsset>()->m_mass);
-	}
+	void reloadProperties_this();
 	void reloadProperties() override { reloadProperties_this(); }
 
 	// update
-	void Update_this()
-	{
-		ModelEditor::Update_this();
-
-		// mass
-		SimpleGUI::TextBox(m_textEditState_mass, Vec2(8, 8));
-		getModel<MoleculeAsset>()->m_mass = Parse<double>(m_textEditState_mass.text);
-		MoveDrawPos(0, 48);
-	}
+	void Update_this();
 	void update() override { Update_this(); }
 };
-*/
