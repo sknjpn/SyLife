f4f82ba17c4045e8aec3b0f3a63c9c40e2fc497a
SKNJPN <sknjpn@gmail.com>
2019-10-10 23:47:50 +0900

    フォーマット

--- SyLife/FieldManager.cpp
+++ SyLife/FieldManager.cpp
@@ -5,5 +5,5 @@ unique_ptr<FieldManager>	g_fieldManagerPtr;
 
 void FieldManager::init()
 {
-	m_size = g_chipManagerPtr->getRect().size* g_chipManagerPtr->getLength();
+	m_size = g_chipManagerPtr->getRect().size * g_chipManagerPtr->getLength();
 }
