45f679c7b96d50ef92b47f9cb669715747292112
SKNJPN <sknjpn@gmail.com>
2019-10-10 14:49:12 +0900

    Materialを取得できるように

--- SyLife/ElementAsset.h
+++ SyLife/ElementAsset.h
@@ -21,6 +21,7 @@ public:
 	double	getMass() const { return m_mass; }
 	double	getRadius() const { return m_radius; }
 	const Color& getColor() const { return m_color; }
+	const Storage& getMaterial() const { return m_material; }
 
 	// JSON
 	void	load_this(const ptree& pt);
