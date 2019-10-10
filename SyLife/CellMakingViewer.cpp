8618ff3a046b7ab28f380b0b6d415f30b773515f
SKNJPN <sknjpn@gmail.com>
2019-10-10 23:27:56 +0900

    初期化の設定

--- SyLife/CellMakingViewer.cpp
+++ SyLife/CellMakingViewer.cpp
@@ -1,7 +1,17 @@
 ﻿#include "CellMakingViewer.h"
 
+#include "ViewerManager.h"
+
+#include "AssemblyViewer.h"
+#include "ReleaseViewer.h"
+#include "PartPaletteViewer.h"
+
 void CellMakingViewer::init()
 {
+	// Viewerの初期化
+	g_viewerManagerPtr->makeViewer<AssemblyViewer>();
+	g_viewerManagerPtr->makeViewer<ReleaseViewer>();
+	g_viewerManagerPtr->makeViewer<PartPaletteViewer>();
 }
 
 void CellMakingViewer::update()
