0c6a36ffed51a2ed8facca93c187e7327791091a
SKNJPN <sknjpn@gmail.com>
2019-10-10 14:52:12 +0900

    合成機のUpdateが正常に行われるように

--- SyLife/SynthesizerState.cpp
+++ SyLife/SynthesizerState.cpp
@@ -5,6 +5,8 @@
 #include "SynthesizerAsset.h"
 #include "PartConfig.h"
 #include "CellState.h"
+#include "CellAsset.h"
+#include "ElementAsset.h"
 
 
 void SynthesizerState::draw(const CellState& cellState) const
@@ -14,16 +16,16 @@ void SynthesizerState::draw(const CellState& cellState) const
 
 void SynthesizerState::update(CellState& cellState)
 {
-	/*
 	m_timer += g_systemManagerPtr->GetDeltaTime();
 
 	auto model = dynamic_pointer_cast<SynthesizerAsset>(getPartConfig()->getModel());
-	if (m_timer > 2.0 && cellState.m_storage >= model->GetImport() && cellState.m_model->getMaterial().numElement(model->GetExport()) > cellState.m_storage.numElement(model->GetExport()))
+	if (m_timer > 2.0 &&
+		cellState.m_storage >= model->getExport()->getMaterial() &&
+		cellState.m_model->getMaxStorage().numElement(model->getExport()) > cellState.m_storage.numElement(model->getExport()))
 	{
 		m_timer = 0.0;
 
-		cellState.m_storage -= model->GetImport();
-		cellState.m_storage.addElement(model->GetExport(), 1);
+		cellState.m_storage -= model->getExport()->getMaterial();
+		cellState.m_storage.addElement(model->getExport());
 	}
-	*/
 }
