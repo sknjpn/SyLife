8224681673c1e270ebbcef21281bbbffe1c21380
SKNJPN <sknjpn@gmail.com>
2019-10-11 16:11:31 +0900

    SlideBarの追加

---
+++ SyLife/SlideBar.cpp
@@ -0,0 +1,6 @@
+﻿#include "stdafx.h"
+#include "SlideBar.h"
+
+void SlideBar::update()
+{
+}
---
+++ SyLife/SlideBar.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+class SlideBar
+{
+	double	m_value;
+	double	m_size;
+
+public:
+
+	void	update();
+	double	getValue() const { return m_value; }
+};
+
