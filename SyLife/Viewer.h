007871e98d3352dde048c90b8fb1962f02e0d2a9
SKNJPN <sknjpn@gmail.com>
2019-10-11 12:09:28 +0900

    invisibleに関する定義

--- SyLife/Viewer.h
+++ SyLife/Viewer.h
@@ -4,6 +4,7 @@ class Viewer
 {
 	friend class ViewerManager;
 
+	bool	m_isInvisible = false;
 	bool	m_isMouseOver = false;
 	int		m_priority = 0;
 	Vec2	m_drawPos = Vec2::Zero();
@@ -15,6 +16,7 @@ public:
 	virtual ~Viewer() = default;
 
 	// Set
+	void	setInvisible(bool isInvisible) { m_isInvisible = isInvisible };
 	void	setPriority(int priority) { m_priority = priority; }
 	void	setBackgroundColor(const Color& color) { m_backgroundColor = color; }
 	void	setDrawPos(const Vec2& pos) { m_drawPos = pos; m_transformer.reset(); m_transformer = make_unique<Transformer2D>(Mat3x2::Translate(m_drawPos), true); }
@@ -31,6 +33,7 @@ public:
 
 	// Get
 	int		getPriority() const { return m_priority; }
+	bool	isInvisible() const { return m_isInvisible; }
 	bool	isMouseOver() const { return m_isMouseOver; }
 	const RectF&	getDrawRect() const { return m_drawRect; }
 	const Vec2&		getDrawSize() const { return m_drawRect.size; }
