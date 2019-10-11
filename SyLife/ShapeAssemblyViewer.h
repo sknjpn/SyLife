a8e684ab38aeb5ce27e180675405dce73437c663
SKNJPN <sknjpn@gmail.com>
2019-10-11 17:36:15 +0900

    実装のコピー

--- SyLife/ShapeAssemblyViewer.h
+++ SyLife/ShapeAssemblyViewer.h
@@ -1,7 +1,20 @@
 ﻿#pragma once
+
 #include "Viewer.h"
+#include "TinyCamera.h"
+
 class ShapeAssemblyViewer :
 	public Viewer
 {
-};
+	TinyCamera	m_camera;
 
+	double	m_width = 300;
+	double	m_height = 300;
+
+public:
+	void	init() override;
+
+	void	update() override;
+
+	void	setSize(const Vec2& size);
+};
