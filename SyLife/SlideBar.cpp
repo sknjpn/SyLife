0473c92239d985aa373b0d20bf8951fc8adb29c2
SKNJPN <sknjpn@gmail.com>
2019-10-11 16:21:48 +0900

    SlideBarの実装作成

--- SyLife/SlideBar.cpp
+++ SyLife/SlideBar.cpp
@@ -2,5 +2,23 @@
 
 void SlideBar::update()
 {
+	Rect(20, m_size).draw(ColorF(0.25)).drawFrame(1.0, 0.0, Palette::White);
+	auto bar = Rect(0, getBarPositionY(), 16, m_length);
 
+	if (m_isDragged)
+	{
+		if (MouseL.up()) m_isDragged = false;
+		else
+		{
+			m_value = Clamp<double>(m_value + Cursor::DeltaF().y / (m_size - m_length), 0.0, 1.0);
+		}
+
+		bar.draw(ColorF(1.0, 1.0));
+	}
+	else
+	{
+		bar.draw(bar.mouseOver() ? ColorF(1.0, 0.75) : ColorF(1.0, 0.5));
+
+		if (bar.leftClicked()) m_isDragged = true;
+	}
 }
--- SyLife/SlideBar.h
+++ SyLife/SlideBar.h
@@ -4,8 +4,18 @@ class SlideBar
 {
 	double	m_value;
 	double	m_size;
+	double	m_length;
+	bool	m_isDragged;
+
+	double	getBarPositionY() const { return (m_size - m_length) * m_value; }
 
 public:
+	SlideBar(double size, double length)
+		: m_size(size)
+		, m_length(length)
+		, m_value(0.0)
+		, m_isDragged(false)
+	{}
 
 	void	update();
 	double	getValue() const { return m_value; }
