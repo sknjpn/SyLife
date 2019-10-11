7230055eb57a54538d6163cac5dc4466f5c6d844
SKNJPN <sknjpn@gmail.com>
2019-10-11 17:44:44 +0900

    関数移動

--- SyLife/ShapeAssemblyViewer.cpp
+++ SyLife/ShapeAssemblyViewer.cpp
@@ -1,2 +1,40 @@
-﻿#include "stdafx.h"
-#include "ShapeAssemblyViewer.h"
+﻿#include "ShapeAssemblyViewer.h"
+
+
+void AssemblyViewer::drawParts() const
+{
+	auto cmv = g_viewerManagerPtr->getViewer<CellMakingViewer>();
+
+	for (const auto& p : cmv->m_cellAsset->getPartConfigs())
+	{
+		auto t2 = Transformer2D(Mat3x2::Rotate(p->getRotation())
+			.translated(p->getPosition().x, p->getPosition().y));
+
+		for (const auto& s : p->getModel()->getShapes())
+			s.m_polygon.draw(ColorF(s.m_color, 0.5)).drawFrame(1.0, Palette::Black);
+	}
+}
+
+void AssemblyViewer::drawGrid() const
+{
+	const int scale = (int)log10(m_camera.getScale());
+	const double thickness = 2.0 / m_camera.getScale();
+	const double interval = pow(10.0, -scale + 1);
+
+	// 縦線
+	{
+		const auto color = ColorF(Palette::White, 0.25);
+
+		for (double x = -m_size.x / 2.0; x <= m_size.x / 2.0; x += interval)
+			Line(x, -m_size.y / 2.0, x, m_size.y / 2.0).draw(thickness, color);
+
+		for (double y = -m_size.y / 2.0; y <= m_size.y / 2.0; y += interval)
+			Line(-m_size.x / 2.0, y, m_size.x / 2.0, y).draw(thickness, color);
+	}
+
+	// XY軸
+	{
+		Line(-m_size.x / 2.0, 0, m_size.x / 2.0, 0).draw(thickness, Palette::Red);
+		Line(0, -m_size.y / 2.0, 0, m_size.y / 2.0).draw(thickness, Palette::Red);
+	}
+}
