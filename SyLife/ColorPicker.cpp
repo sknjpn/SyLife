c010695343d18d590a240fdf12428abd9b358a10
SKNJPN <sknjpn@gmail.com>
2019-10-12 11:06:00 +0900

    色合い修正

--- SyLife/ColorPicker.cpp
+++ SyLife/ColorPicker.cpp
@@ -11,7 +11,7 @@ ColorPicker::ColorPicker()
 		for (auto p : step(size))
 		{
 			if ((p - size / 2).length() < length / 2)
-				image[p] = HSV(ToDegrees(atan2(p.y, p.x)), (p - size / 2).length() / (length / 2));
+				image[p] = HSV(ToDegrees(atan2(p.y, p.x)), (p - size / 2).length() / (length / 2), 1.0);
 		}
 		m_circleTexture = Texture(image);
 	}
