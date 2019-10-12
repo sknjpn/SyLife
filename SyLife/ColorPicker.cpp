11eb64483bf90f2bcec0f345f3c02f97e2a6210d
SKNJPN <sknjpn@gmail.com>
2019-10-12 11:06:59 +0900

    差の表現修正

--- SyLife/ColorPicker.cpp
+++ SyLife/ColorPicker.cpp
@@ -10,8 +10,10 @@ ColorPicker::ColorPicker()
 		Image image(size);
 		for (auto p : step(size))
 		{
-			if ((p - size / 2).length() < length / 2)
-				image[p] = HSV(ToDegrees(atan2(p.y, p.x)), (p - size / 2).length() / (length / 2), 1.0);
+			const Point d = (p - size / 2);
+
+			if (d.length() < length / 2)
+				image[p] = HSV(ToDegrees(atan2()), d.length() / (length / 2), 1.0);
 		}
 		m_circleTexture = Texture(image);
 	}
