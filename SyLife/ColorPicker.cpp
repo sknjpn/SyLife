7ef61a1bc8e8b25cf7886954fa5610e429dc1b27
SKNJPN <sknjpn@gmail.com>
2019-10-12 11:04:50 +0900

    degreeのバグ修正

--- SyLife/ColorPicker.cpp
+++ SyLife/ColorPicker.cpp
@@ -11,7 +11,7 @@ ColorPicker::ColorPicker()
 		for (auto p : step(size))
 		{
 			if ((p - size / 2).length() < length / 2)
-				image[p] = HSV(atan2(p.y, p.x) * Math::Pi, (p - size / 2).length() / (length / 2));
+				image[p] = HSV(ToDegrees(atan2(p.y, p.x)), (p - size / 2).length() / (length / 2));
 		}
 		m_circleTexture = Texture(image);
 	}
