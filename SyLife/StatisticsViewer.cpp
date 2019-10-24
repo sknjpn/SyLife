394092b71721d5c30b2217f96a96de20811fd88d
SKNJPN <sknjpn@gmail.com>
2019-10-24 20:12:30 +0900

    Logの更新処理の追加

--- SyLife/StatisticsViewer.cpp
+++ SyLife/StatisticsViewer.cpp
@@ -1,11 +1,27 @@
 ﻿#include "StatisticsViewer.h"
 
+#include "CellAsset.h"
+#include "CellMakingViewer.h"
+
+#include "AssetManager.h"
+
 void StatisticsViewer::init()
 {
 }
 
 void StatisticsViewer::update()
 {
+	// Logの追加
+	auto cas = g_assetManagerPtr->getAssets<CellAsset>();
+	for (auto& ca : cas)
+		if (m_logs.all([&ca](const auto& l) { return l.m_cellAsset != ca; }))
+			m_logs.emplace_back(ca);
+
+	// Logの更新
+	for (auto& l : m_logs)
+		l.m_statuses.emplace_back();
+
+	// Logの表示
 }
 
 StatisticsViewer::Log::Status::Status()
