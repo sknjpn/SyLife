3c35ba8f90a4acbca577ab3fa9495a9047b0e873
SKNJPN <sknjpn@gmail.com>
2019-10-24 20:35:45 +0900

    Logの表示実装

--- SyLife/StatisticsViewer.cpp
+++ SyLife/StatisticsViewer.cpp
@@ -1,12 +1,13 @@
 ﻿#include "StatisticsViewer.h"
 
 #include "CellAsset.h"
-#include "CellMakingViewer.h"
+#include "CellManager.h"
 
 #include "AssetManager.h"
 
 void StatisticsViewer::init()
 {
+	setDrawSize(m_statusesSizeMax, 100);
 }
 
 void StatisticsViewer::update()
@@ -23,9 +24,22 @@ void StatisticsViewer::update()
 		l.m_statuses.emplace_back();
 		while (l.m_statuses.size() > m_statusesSizeMax) l.m_statuses.pop_front();
 	}
+
 	// Logの表示
+	for (auto& l : m_logs)
+	{
+		int max = 1;
+		for (auto& s : l.m_statuses) max = Max(max, s.m_num);
+		for (int i = 0; i < l.m_statuses.size() - 1; ++i)
+		{
+			Vec2 p0(i, l.m_statuses[i].m_num * 100.0 / max);
+			Vec2 p1(i + 1, l.m_statuses[i + 1].m_num * 100.0 / max);
+			Line(p0, p1).draw();
+		}
+	}
 }
 
 StatisticsViewer::Log::Status::Status()
 {
+	m_num = g_cellManagerPtr->getCellStates().size();
 }
