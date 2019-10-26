73ae34bd1b161532060a657ecb1932389e612df5
SKNJPN <sknjpn@gmail.com>
2019-10-26 17:43:59 +0900

    インデント

--- SyLife/Asset.h
+++ SyLife/Asset.h
@@ -21,7 +21,7 @@ public:
 
 	// JSON
 	void	load_this(const ptree& pt);
-	 void	load(const ptree& pt) override { load_this(pt); }
+	void	load(const ptree& pt) override { load_this(pt); }
 	void	save_this(ptree& pt) const;
-	 void	save(ptree& pt) const override{ save_this(pt); }
+	void	save(ptree& pt) const override { save_this(pt); }
 };
