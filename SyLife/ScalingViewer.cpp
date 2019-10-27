dc5baa10539eb1126b8948cd73c23d6b701b6fff
SKNJPN <sknjpn@gmail.com>
2019-10-27 14:32:44 +0900

    update

--- SyLife/ScalingViewer.cpp
+++ SyLife/ScalingViewer.cpp
@@ -2,9 +2,13 @@
 
 void ScalingViewer::init()
 {
+	setBackgroundColor(Color(0, 0));
 	setDrawSize(80, 320);
 }
 
 void ScalingViewer::update()
 {
+	Rect(80, 320).rounded(4)
+		.draw(Palette::White)
+		.drawFrame(2.0, 0.0, Palette::Black);
 }
