106bb7448dc5f194cc533f614019b4a53c0db63e
SKNJPN <sknjpn@gmail.com>
2019-10-27 14:30:45 +0900

    大きさ設定

--- SyLife/ScalingViewer.cpp
+++ SyLife/ScalingViewer.cpp
@@ -2,6 +2,7 @@
 
 void ScalingViewer::init()
 {
+	setDrawSize(80, 320);
 }
 
 void ScalingViewer::update()
--- SyLife/ScalingViewer.h
+++ SyLife/ScalingViewer.h
@@ -6,6 +6,11 @@ class ScalingViewer :
 	public Viewer
 {
 public:
+	ScalingViewer()
+	{
+		setDrawSize(80, 320);
+	}
+
 	void	init() override;
 	void	update() override;
 };
