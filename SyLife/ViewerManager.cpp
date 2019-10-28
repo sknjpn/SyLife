665785e305a74a8aad2ad232bd02f075dafa0355
SKNJPN <sknjpn@gmail.com>
2019-10-28 23:14:44 +0900

    新たなViewer処理の実装

--- SyLife/ViewerManager.cpp
+++ SyLife/ViewerManager.cpp
@@ -16,6 +16,42 @@ ViewerManager::ViewerManager()
 
 void ViewerManager::update()
 {
+	// ログ出力のクリア
+	ClearPrint();
+
+	// Viewerのリセット
+	auto viewers = m_rootViewer->getAllChildViewers();
+	shared_ptr<Viewer> mouseoverViewer;
+	for (auto it = viewers.begin(); it < viewers.end(); ++it)
+	{
+		(*it)->m_isMouseover = false;
+		(*it)->m_drawPos = Vec2::Zero();
+
+		if ((*it)->m_viewerRect.mouseOver()) mouseoverViewer = *it;
+	}
+
+	// mouseOver 適用
+	if (mouseoverViewer) mouseoverViewer->m_isMouseover = true;
+
+	// Viewer 更新
+	for (auto it = viewers.begin(); it < viewers.end(); ++it)
+	{
+		// 消されたならば処理しない
+		if ((*it)->m_isDestroyed) continue;
+
+		auto vp = ScopedViewport2D(Rect((*it)->m_viewerRect));
+		auto t = Transformer2D(Mat3x2::Identity(), Mat3x2::Translate((*it)->m_viewerRect.pos));
+
+		RectF((*it)->m_viewerRect.size).draw((*it)->m_backgroundColor);
+
+		(*it)->update();
+
+		(*it)->m_transformer.reset();
+
+		// フレーム描画
+		if (KeyF.pressed()) RectF((*it)->m_viewerRect.size).drawFrame(1.0, 0.0, ColorF(Palette::Red, 0.5));
+	}
+
 	// destroyされたViewerの削除
 	{
 		auto viewers = m_rootViewer->getAllChildViewers();
@@ -38,60 +74,4 @@ void ViewerManager::update()
 		}
 	}
 
-	// ログ出力のクリア
-	ClearPrint();
-
-	auto viewers = m_rootViewer->getAllChildViewers();
-	// Viewerのリセット
-	for (auto it = viewers.begin(); it < viewers.end(); ++it)
-	{
-		(*it)->m_isMouseover = false;
-		(*it)->m_drawPos = Vec2::Zero();
-	}
-
-	// mouseOver 適用
-	for (auto it = m_viewers.rbegin(); it < m_viewers.rend(); ++it)
-	{
-		if (!(*it)->isInvisible() && (*it)->m_viewerRect.mouseOver())
-		{
-			(*it)->m_isMouseover = true;
-
-			break;
-		}
-	}
-
-	// Viewer 更新
-	for (auto it = m_viewers.begin(); it < m_viewers.end(); ++it)
-	{
-		// Viewerが消されてnullptrになっている可能性がある
-		if (*it == nullptr) continue;
-
-		if ((*it)->isInvisible())
-		{
-			auto vp = ScopedViewport2D(0, 0);
-
-			(*it)->update();
-
-			// Viewerが消されてnullptrになっている可能性がある
-			if (*it != nullptr) (*it)->m_transformer.reset();
-		}
-		else
-		{
-			auto vp = ScopedViewport2D(Rect((*it)->m_viewerRect));
-			auto t = Transformer2D(Mat3x2::Identity(), Mat3x2::Translate((*it)->m_viewerRect.pos));
-
-			RectF((*it)->m_viewerRect.size).draw((*it)->m_backgroundColor);
-
-			(*it)->update();
-
-			if (*it != nullptr)
-			{
-				// Viewerが消されてnullptrになっている可能性がある
-				(*it)->m_transformer.reset();
-
-				// フレーム描画
-				RectF((*it)->m_viewerRect.size).drawFrame(1.0, 0.0, ColorF(Palette::Red, 0.5));
-			}
-		}
-	}
 }
