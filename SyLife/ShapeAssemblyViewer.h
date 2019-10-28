0135fc05576240f12c2fc1b6ca2a589fcc44c9e8
SKNJPN <sknjpn@gmail.com>
2019-10-28 18:50:39 +0900

    コンストラクタに移動

--- SyLife/ShapeAssemblyViewer.h
+++ SyLife/ShapeAssemblyViewer.h
@@ -14,7 +14,16 @@ class ShapeAssemblyViewer
 	shared_ptr<PartAsset>	m_partAsset;
 
 public:
-	void	init() override;
+	ShapeAssemblyViewer()
+	{
+		setBackgroundColor(Palette::Gray);
+
+		setPriority(2);
+
+		setViewerRect(RectF(800, 800).setCenter(getDrawCenter().movedBy(0, -50)));
+		setSize(Vec2(800, 800));
+	}
+
 	void	update() override;
 
 	void	setPartAsset(const shared_ptr<PartAsset>& partAsset) { m_partAsset = partAsset; }
