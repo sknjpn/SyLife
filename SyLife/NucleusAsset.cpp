927aed0979c7d2e1ada6102540139fff93a62a24
SKNJPN <sknjpn@gmail.com>
2019-10-31 08:11:45 +0900

    Nucleusに時間情報の追加

--- SyLife/NucleusAsset.cpp
+++ SyLife/NucleusAsset.cpp
@@ -18,11 +18,19 @@ shared_ptr<PartState> NucleusAsset::makeState()
 void NucleusAsset::load(const ptree& pt)
 {
 	PartAsset::load(pt);
+
+	m_lifespanTime = pt.get<double>("lifespanTime");
+	m_bornTime = pt.get<double>("bornTime");
+	m_yieldTime = pt.get<double>("yieldTime");
 }
 
 void NucleusAsset::save(ptree& pt) const
 {
 	PartAsset::save(pt);
+
+	pt.put("lifespanTime", m_lifespanTime);
+	pt.put("bornTime", m_bornTime);
+	pt.put("yieldTime", m_yieldTime);
 
 	// type
 	pt.put("type", "NucleusAsset");
--- SyLife/NucleusAsset.h
+++ SyLife/NucleusAsset.h
@@ -5,6 +5,10 @@
 class NucleusAsset
 	: public PartAsset
 {
+	double	m_lifespanTime;	// 寿命
+	double	m_bornTime;		// 生まれるまでの時間
+	double	m_yieldTime;	// 生むのにかかる時間
+
 public:
 	void makeViewer() override;
 	shared_ptr<PartState>	makeState() override;
