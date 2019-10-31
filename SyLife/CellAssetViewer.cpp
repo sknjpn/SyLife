f0ada68376e9f199605590a37c1813536953964d
SKNJPN <sknjpn@gmail.com>
2019-10-31 11:17:08 +0900

    CellAssetViewreの実装

--- SyLife/CellAssetViewer.cpp
+++ SyLife/CellAssetViewer.cpp
@@ -1,2 +1,49 @@
-﻿#include "stdafx.h"
-#include "CellAssetViewer.h"
+﻿#include "CellAssetViewer.h"
+#include "CellAsset.h"
+#include "PartConfig.h"
+#include "PartAsset.h"
+
+CellAssetViewer::CellAssetViewer(const shared_ptr<CellAsset>& cellAsset)
+	: m_cellAsset(cellAsset)
+{
+	setViewerSize(200, 800);
+}
+
+void CellAssetViewer::update()
+{
+	// Font
+	static Font font(20);
+	const double d = 25;
+
+	// 背景
+	{
+		RectF rect(getViewerSize());
+		rect.stretched(-10).draw(Palette::White).drawFrame(5.0, 0.0, Palette::Black);
+	}
+
+	if (!m_cellAsset->isValid())
+	{
+		font(U"有効でないCellAsset").draw();
+		return;
+	}
+
+	font(U"CellAsset").draw();
+	moveDrawPos(0, d);
+	font(U"Name=" + Unicode::Widen(m_cellAsset->getName())).draw();
+	moveDrawPos(0, d);
+	font(U"Mass=", m_cellAsset->getMass()).draw();
+	moveDrawPos(0, d);
+	font(U"Radius=", m_cellAsset->getRadius()).draw();
+	moveDrawPos(0, d);
+	font(U"Inertia=", m_cellAsset->getInertia()).draw();
+	moveDrawPos(0, d);
+	font(U"NumParts=", m_cellAsset->getPartConfigs().size()).draw();
+	moveDrawPos(0, d);
+	font(U"Parts").draw();
+	moveDrawPos(0, d);
+	for (auto it = m_cellAsset->getPartConfigs().begin(); it != m_cellAsset->getPartConfigs().end(); ++it)
+	{
+		font(U"PartName=" + Unicode::Widen((*it)->getPartAsset()->getName())).draw();
+		moveDrawPos(0, d);
+	}
+}
--- SyLife/CellAssetViewer.h
+++ SyLife/CellAssetViewer.h
@@ -1,7 +1,20 @@
 ﻿#pragma once
+
 #include "Viewer.h"
-class CellAssetViewer :
-	public Viewer
+
+class CellAsset;
+
+class CellAssetViewer
+	: public Viewer
 {
+	shared_ptr<CellAsset>	m_cellAsset;
+
+public:
+	CellAssetViewer(const shared_ptr<CellAsset>& cellAsset);
+
+	// get
+	const shared_ptr<CellAsset>& getCellAsset() const { return m_cellAsset; }
+
+	void	update() override;
 };
 
