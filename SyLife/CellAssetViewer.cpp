852f59e06c90f92a0c0b78a658e86b04ccfb7e90
SKNJPN <sknjpn@gmail.com>
2019-10-31 17:30:00 +0900

    表示の調整

--- SyLife/CellAssetViewer.cpp
+++ SyLife/CellAssetViewer.cpp
@@ -6,44 +6,46 @@
 CellAssetViewer::CellAssetViewer(const shared_ptr<CellAsset>& cellAsset)
 	: m_cellAsset(cellAsset)
 {
-	setViewerSize(200, 800);
+	setViewerSize(250, 800);
 }
 
 void CellAssetViewer::update()
 {
 	// Font
-	static Font font(20);
-	const double d = 25;
+	static Font font(16, Typeface::Bold);
+	const double d = 20;
 
 	// 背景
 	{
 		RectF rect(getViewerSize());
-		rect.stretched(-10).draw(Palette::White).drawFrame(5.0, 0.0, Palette::Black);
+		rect.rounded(10).draw(Palette::White).drawFrame(5.0, 0.0, Palette::Black);
 	}
+
+	moveDrawPos(10, 10);
 
 	if (!m_cellAsset->isValid())
 	{
-		font(U"有効でないCellAsset").draw();
+		font(U"有効でないCellAsset").draw(Vec2::Zero(), Palette::Black);
 		return;
 	}
 
-	font(U"CellAsset").draw();
+	font(U"CellAsset").draw(Vec2::Zero(), Palette::Black);
 	moveDrawPos(0, d);
-	font(U"Name=" + Unicode::Widen(m_cellAsset->getName())).draw();
+	font(U"Name=" + Unicode::Widen(m_cellAsset->getName())).draw(Vec2::Zero(), Palette::Black);
 	moveDrawPos(0, d);
-	font(U"Mass=", m_cellAsset->getMass()).draw();
+	font(U"Mass=", m_cellAsset->getMass()).draw(Vec2::Zero(), Palette::Black);
 	moveDrawPos(0, d);
-	font(U"Radius=", m_cellAsset->getRadius()).draw();
+	font(U"Radius=", m_cellAsset->getRadius()).draw(Vec2::Zero(), Palette::Black);
 	moveDrawPos(0, d);
-	font(U"Inertia=", m_cellAsset->getInertia()).draw();
+	font(U"Inertia=", m_cellAsset->getInertia()).draw(Vec2::Zero(), Palette::Black);
 	moveDrawPos(0, d);
-	font(U"NumParts=", m_cellAsset->getPartConfigs().size()).draw();
+	font(U"NumParts=", m_cellAsset->getPartConfigs().size()).draw(Vec2::Zero(), Palette::Black);
 	moveDrawPos(0, d);
-	font(U"Parts").draw();
+	font(U"Parts").draw(Vec2::Zero(), Palette::Black);
 	moveDrawPos(0, d);
 	for (auto it = m_cellAsset->getPartConfigs().begin(); it != m_cellAsset->getPartConfigs().end(); ++it)
 	{
-		font(U"PartName=" + Unicode::Widen((*it)->getPartAsset()->getName())).draw();
+		font(U"PartName=" + Unicode::Widen((*it)->getPartAsset()->getName())).draw(Vec2::Zero(), Palette::Black);
 		moveDrawPos(0, d);
 	}
 }
