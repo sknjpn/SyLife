73c34ff2bd20dd2052c5ce9234fd13ceff246d8d
SKNJPN <sknjpn@gmail.com>
2019-12-25 10:15:54 +0900

    remove sound

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,9 @@
 ﻿#include "SetupViewer.h"
-#include "SoundManager.h"
 #include "World.h"
 
 void Main()
 {
 	Window::SetTitle(U"SyLife");
-
-	g_soundManagerPtr = MakeUnique<SoundManager>();
 
 	Viewer::GetRootViewer()->addChildViewer<SetupViewer>();
 	Viewer::Run();
