5c8b4f201aa10a7c63abc7102c4749ec0fd76d69
SKNJPN <sknjpn@gmail.com>
2019-12-25 10:21:13 +0900

    remove setup

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,13 @@
-﻿#include "SetupViewer.h"
+﻿#include "WorldGenerateViewer.h"
 #include "World.h"
 
 void Main()
 {
 	Window::SetTitle(U"SyLife");
+	Window::Resize(1920, 1080);
+	Window::SetStyle(WindowStyle::Sizable);
+	Window::Maximize();
 
-	Viewer::GetRootViewer()->addChildViewer<SetupViewer>();
+	Viewer::GetRootViewer()->addChildViewer<WorldGenerateViewer>();
 	Viewer::Run();
 }
