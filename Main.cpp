a0be2ce60dc06c08696d07097e08ed027452ec33
SKNJPN <sknjpn@gmail.com>
2020-01-07 23:32:37 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@ void Main()
 	Window::SetStyle(WindowStyle::Sizable);
 	Scene::Resize(1920, 1080);
 	Window::Maximize();
-	Window::SetFullscreen(true);
+	//Window::SetFullscreen(true);
 	// world生成
 	if (FileSystem::Exists(U"world/"))
 	{
