10045deaae58b694cf8deec25b922261a08baee8
SKNJPN <sknjpn@gmail.com>
2020-01-23 20:41:05 +0900

    UI調整

--- MainViewer_CellMakingViewer_CellInfo.cpp
+++ MainViewer_CellMakingViewer_CellInfo.cpp
@@ -22,51 +22,26 @@ void MainViewer::CellMakingViewer::CellInfo::update()
 		moveDrawPos(0, 40);
 	}
 
-	// Release
-	{
-		const RectF rect = Rect(300, 300).stretched(-5);
-		const double r = rect.size.x / 2.0;
-
-		Circle(rect.size / 2.0, r)
-			.draw(Palette::Skyblue)
-			.drawFrame(2.0, 0.0, Palette::Black);
-
-		// part
-		{
-			auto t1 = Transformer2D(Mat3x2::Translate(-cellAsset->getCentroid()).scaled(r / cellAsset->getRadius() / 2.0).translated(rect.center()));
-
-			for (const auto& partConfig : cellAsset->getPartConfigs())
-			{
-				auto t2 = Transformer2D(partConfig->getMat3x2());
-
-				partConfig->getPartAsset()->getShape().draw(0.5);
-				partConfig->getPartAsset()->getShape().getPolygon().drawFrame(2.0, Palette::Black);
-			}
-		}
-
-		moveDrawPos(0, 300);
-	}
-
 	// material
 	{
 		static Font font(16, Typeface::Bold);
 
 		// Proteins
-		font(U"この生き物を作るのに必要なもの").draw();
+		font(U"この生き物を作るのに必要なもの").draw(Vec2::Zero(), Palette::Black);
 		moveDrawPos(0, 20);
 		{
 			moveDrawPos(8, 0);
 
-			font(U"エレメント(緑色で広がっているもの)").draw();
+			font(U"エレメント(緑色で広がっているもの)").draw(Vec2::Zero(), Palette::Black);
 			{
 				moveDrawPos(8, 0);
 				moveDrawPos(0, 20);
-				font(ToString(int(cellAsset->getMaterial().getElement())) + U"個").draw();
+				font(ToString(int(cellAsset->getMaterial().getElement())) + U"個").draw(Vec2::Zero(), Palette::Black);
 				moveDrawPos(0, 30);
 				moveDrawPos(-8, 0);
 			}
 
-			font(U"プロテイン(合成器官で出来るもの)").draw();
+			font(U"プロテイン(合成器官で出来るもの)").draw(Vec2::Zero(), Palette::Black);
 			{
 				moveDrawPos(8, 0);
 				for (const auto& protein : cellAsset->getMaterial().getProteinList())
@@ -81,13 +56,13 @@ void MainViewer::CellMakingViewer::CellInfo::update()
 			moveDrawPos(-8, 0);
 		}
 
-		font(U"この生き物が作れるプロテイン").draw();
+		font(U"この生き物が作れるプロテイン").draw(Vec2::Zero(), Palette::Black);
 
-		font(U"この生き物の特性(核で決まる)").draw();
-		font(U"寿命").draw();
-		font(U"増殖までの時間").draw();
-		font(U"硬さ").draw();
-		font(U"攻撃力").draw();
-		font(U"移動力").draw();
+		font(U"この生き物の特性(核で決まる)").draw(Vec2::Zero(), Palette::Black);
+		font(U"寿命").draw(Vec2::Zero(), Palette::Black);
+		font(U"増殖までの時間").draw(Vec2::Zero(), Palette::Black);
+		font(U"硬さ").draw(Vec2::Zero(), Palette::Black);
+		font(U"攻撃力").draw(Vec2::Zero(), Palette::Black);
+		font(U"移動力").draw(Vec2::Zero(), Palette::Black);
 	}
 }
