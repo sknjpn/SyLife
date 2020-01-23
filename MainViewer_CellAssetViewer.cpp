cf4829e0b080f7f5217e5e2c13e878679792cc49
SKNJPN <sknjpn@gmail.com>
2020-01-24 07:31:52 +0900

    表示の追加

--- MainViewer_CellAssetViewer.cpp
+++ MainViewer_CellAssetViewer.cpp
@@ -1,5 +1,7 @@
 ﻿#include "MainViewer.h"
 #include "CellAsset.h"
+#include "GUIText.h"
+#include "GUITextBox.h"
 
 MainViewer::CellAssetViewer::CellAssetViewer(const shared_ptr<CellAsset>& cellAsset)
 {
@@ -7,8 +9,31 @@ MainViewer::CellAssetViewer::CellAssetViewer(const shared_ptr<CellAsset>& cellAs
 
 void MainViewer::CellAssetViewer::init()
 {
+	addChildViewer<GUIText>(m_cellAsset->getNameJP(), Font(24, Typeface::Heavy), GUIText::)
+		->setViewerRectInLocal(5, 5, 128, 40);
 }
 
 void MainViewer::CellAssetViewer::update()
 {
+	RectF(getViewerSize()).rounded(5.0).draw(ColorF(1.0)).drawFrame(2.0, 0.0, Palette::Black);
+
+	// name
+	{
+		
+	}
+
+	// 数の推移
+	{
+
+	}
+
+	// アイコン
+	{
+
+	}
+
+	// 性能
+	{
+
+	}
 }
