bc24f124702f36bcf6821cb6a5ee0a4bc5c0ab1f
SKNJPN <sknjpn@gmail.com>
2020-01-24 07:32:43 +0900

    GUITextの更新

--- GUIText.cpp
+++ GUIText.cpp
@@ -2,15 +2,27 @@
 
 void GUIText::update()
 {
-	static const Font font(128, Typeface::Bold);
-
 	const RectF rect(getViewerSize());
 
 	// 文字列描画
+	switch (m_mode)
 	{
-		const auto s = 0.75 * rect.h / 128.0;
-		const auto t = Transformer2D(Mat3x2::Scale(s));
-
-		font(m_text).drawAt(rect.center() / s, Palette::Black);
+	case GUIText::Mode::DrawAtCenter:
+	{
+		m_font(m_text).drawAt(rect.center(), m_color);
+	}
+	break;
+	case GUIText::Mode::DrawInBox:
+	{
+		m_font(m_text).draw(rect, m_color);
+	}
+	break;
+	case GUIText::Mode::DrawLeftCenter:
+	{
+		m_font(m_text).draw(Arg::leftCenter(0.0, getViewerSize().y / 2.0), m_color);
+	}
+	break;
+	default:
+		break;
 	}
 }
--- GUIText.h
+++ GUIText.h
@@ -1,15 +1,28 @@
 ﻿#pragma once
 
-#include "Viewer.h"
+#include "EasyViewer.h"
 
 class GUIText :
-	public Viewer
+	public EasyViewer
 {
+public:
 	String	m_text;
+	Font	m_font;
+	Color	m_color;
+
+	enum class Mode
+	{
+		DrawAtCenter,
+		DrawInBox,
+		DrawLeftCenter,
+	} m_mode;
 
 public:
-	GUIText(const String& text)
+	GUIText(const String& text, const Font& font, Mode mode = Mode::DrawAtCenter, Color color = Palette::Black)
 		: m_text(text)
+		, m_font(font)
+		, m_mode(mode)
+		, m_color(color)
 	{}
 
 	void	update() override;
