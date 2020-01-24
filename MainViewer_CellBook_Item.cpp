96480001f03ab239aedb6cf8d97573c723c3667e
SKNJPN <sknjpn@gmail.com>
2020-01-24 10:42:04 +0900

    CellBookがAssetViewerに対応するように

--- MainViewer_CellBook_Item.cpp
+++ MainViewer_CellBook_Item.cpp
@@ -31,6 +31,9 @@ void MainViewer::CellBook::Item::update()
 		if (MouseL.down())
 		{
 			getParentViewer()->getParentViewer()->getChildViewer<FieldViewer>()->release(m_cellAsset);
+
+			if (!getParentViewer()->getParentViewer<MainViewer>()->hasChildViewer(m_cellAsset->getName()))
+				getParentViewer()->getParentViewer<MainViewer>()->addChildViewer<CellAssetViewer>(m_cellAsset);
 		}
 	}
 }
