543cfb59658264d5d942c3f7f0eabd643faf5543
SKNJPN <sknjpn@gmail.com>
2020-01-25 15:21:00 +0900

    DraggableViewerの統合

--- DraggableViewer.h
+++ DraggableViewer.h
@@ -10,6 +10,30 @@ class DraggableViewer :
 	Vec2	m_deltaPos;
 
 public:
-	void	update() override;
+	void	update() override
+	{
+		if (isMouseover() && MouseL.down())
+		{
+			m_isGrabbed = true;
+			m_grabbedPos = Cursor::PosF();
+			m_deltaPos = Vec2::Zero();
+
+			moveToFront();
+		}
+
+		if (MouseL.up()) m_isGrabbed = false;
+
+		if (m_isGrabbed)
+		{
+			setViewerPosInLocal(getViewerPosInLocal() + Cursor::DeltaF());
+
+			if (getViewerPosInLocal().x < 0) setViewerPosInLocal(0, getViewerPosInLocal().y);
+			if (getViewerPosInLocal().y < 0) setViewerPosInLocal(getViewerPosInLocal().x, 0);
+
+			auto d = getViewerPosInLocal() + getViewerSize() - getParentViewer()->getViewerSize();
+			if (d.x > 0) setViewerPosInLocal(getViewerPosInLocal().x - d.x, getViewerPosInLocal().y);
+			if (d.y > 0) setViewerPosInLocal(getViewerPosInLocal().x, getViewerPosInLocal().y - d.y);
+		}
+	}
 };
 
