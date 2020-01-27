fa6ebdc3b4c82bb4cea1f4d52af9b187ce830816
SKNJPN <sknjpn@gmail.com>
2020-01-27 21:11:16 +0900

    HiddenViewerの追加

---
+++ HiddenViewer.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+#include "EasyViewer.h"
+
+class HiddenViewer : public EasyViewer
+{
+	Vec2	m_firstPosInLocal;
+	Vec2	m_secondPosInLocal;
+
+	enum class Mode
+	{
+		None,
+		MoveToFirstPos,
+		MoveToSecondPos,
+	} m_mode = Mode::None;
+
+public:
+	std::shared_ptr<HiddenViewer> setFirstPosInLocal(const Vec2& pos)
+	{
+		if (m_mode == Mode::None)
+		{
+			m_mode = Mode::MoveToFirstPos;
+			setViewerPosInLocal(pos);
+		}
+
+		m_firstPosInLocal = pos; return std::dynamic_pointer_cast<HiddenViewer>(shared_from_this());
+	}
+
+	std::shared_ptr<HiddenViewer> setSecondPosInLocal(const Vec2& pos) { m_secondPosInLocal = pos; return std::dynamic_pointer_cast<HiddenViewer>(shared_from_this()); }
+
+	void moveToFirstPos() { m_mode = Mode::MoveToFirstPos; }
+	void moveToSecondPos() { m_mode = Mode::MoveToSecondPos; }
+
+	void update() override
+	{
+		const Vec2 posInLocal = getViewerPosInLocal();
+		const double rate = 0.1;
+
+		switch (m_mode)
+		{
+		case HiddenViewer::Mode::None:
+			break;
+		case HiddenViewer::Mode::MoveToFirstPos:
+		{
+			setViewerPosInLocal(posInLocal.lerp(m_firstPosInLocal, rate));
+		}
+			break;
+		case HiddenViewer::Mode::MoveToSecondPos:
+		{
+			setViewerPosInLocal(posInLocal.lerp(m_firstPosInLocal, rate));
+		}
+			break;
+		default:
+			break;
+		}
+	}
+};
