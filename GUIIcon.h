2281d5f4512e0df3d9e1be32159b698a44f9ae6c
SKNJPN <sknjpn@gmail.com>
2020-01-27 21:38:42 +0900

    GUIIconの追加

---
+++ GUIIcon.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "EasyViewer.h"
+
+class GUIButtonIcon : public EasyViewer
+{
+	Color	m_color;
+	uint32	m_code;
+	Texture	m_texture;
+	double	m_scale;
+
+public:
+	GUIButtonIcon(uint32 code)
+	{
+		setIcon(code);
+	}
+
+	void	setIcon(uint32 code) { m_code = code; m_texture = Texture(Icon(m_code, Min(getViewerSize().x, getViewerSize().y))); }
+
+	void	init() override
+	{
+		mouseoverDisable();
+	}
+
+	void	update() override
+	{
+		if (getViewerSize() != m_texture.size()) setIcon(m_code);
+		
+		// Icon描画
+		m_texture.drawAt(getViewerSize() / 2.0, m_color);
+	}
+};
