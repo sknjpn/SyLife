5d9e9a6b753c9fa146ab80aed0a86174efeb754f
sknjpn <sknjpn@gmail.com>
2020-02-14 12:20:26 +0900

    nameの編集BOX作成

--- Asset_Editor.cpp
+++ Asset_Editor.cpp
@@ -5,11 +5,16 @@
 
 void Asset_Editor::init()
 {
-	// 名前
-	addChildViewer<GUIText>(U"名前", Font(18), GUIText::Mode::DrawLeftCenter)
-		->setViewerRectInLocal(10, 10, 50, 30);
-	addChildViewer<GUITextBox>()
+	// name
+	{
+		addChildViewer<GUIText>(U"名前", Font(18), GUIText::Mode::DrawLeftCenter)
+			->setName(U"name")
+			->setViewerRectInLocal(10, 10, 50, 30);
 
+		addChildViewer<GUITextBox>(m_asset->getName())
+			->setName(U"name")
+			->setViewerRectInLocal(60, 10, 100, 30);
+	}
 }
 
 void Asset_Editor::update()
@@ -18,6 +23,7 @@ void Asset_Editor::update()
 
 void Asset_Editor::apply() const
 {
-	//m_asset->setName(getChildViewer<GUITextBox>(U"name")->getv);
-	//m_asset->setNameJP()
+	m_asset->setName(getChildViewer<GUITextBox>(U"name")->getValue());
+	m_asset->setNameJP(getChildViewer<GUITextBox>(U"nameJP")->getValue());
+	m_asset->setFilePath(getChildViewer<GUITextBox>(U"filepath")->getValue());
 }
