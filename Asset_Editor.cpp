2fc714b908d8c5378693accd59d4cd86d5a49c13
sknjpn <sknjpn@gmail.com>
2020-02-24 11:03:57 +0900

    Update Asset_Editor.cpp

--- Asset_Editor.cpp
+++ Asset_Editor.cpp
@@ -7,13 +7,13 @@ void Asset_Editor::init()
 {
 	// name
 	{
-		addChildViewer<GUIText>(U"名前", Font(18), GUIText::Mode::DrawLeftCenter)
+/*		addChildViewer<GUIText>(U"名前", Font(18), GUIText::Mode::DrawLeftCenter)
 			->setName(U"name")
 			->setViewerRectInLocal(10, 10, 50, 30);
 
 		addChildViewer<GUITextBox>(m_asset->getName())
 			->setName(U"name")
-			->setViewerRectInLocal(60, 10, 100, 30);
+			->setViewerRectInLocal(60, 10, 100, 30);*/
 	}
 }
 
@@ -23,7 +23,7 @@ void Asset_Editor::update()
 
 void Asset_Editor::apply() const
 {
-	m_asset->setName(getChildViewer<GUITextBox>(U"name")->getValue());
-	m_asset->setNameJP(getChildViewer<GUITextBox>(U"nameJP")->getValue());
-	m_asset->setFilePath(getChildViewer<GUITextBox>(U"filepath")->getValue());
+	//m_asset->setName(getChildViewer<GUITextBox>(U"name")->getValue());
+	//m_asset->setNameJP(getChildViewer<GUITextBox>(U"nameJP")->getValue());
+	//m_asset->setFilePath(getChildViewer<GUITextBox>(U"filepath")->getValue());
 }
