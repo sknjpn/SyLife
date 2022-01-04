e840e7c2a81f20a9f46b533464c73cc46af57517
sknjpn <sknjpn@gmail.com>
2022-01-04 23:51:04 +0900

    fix asset load

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,8 @@ void Main()
 	// loadBGM
 	if (GeneralSetting::GetInstance().m_audioEnabled)
 	{
-#ifdef _WIN32
+		// TODO:Enable Asset Load
+/*#ifdef _WIN32
 		AudioAsset::Register(U"天のきざはし", Resource(U"resources/music/天のきざはし.mp3"), AssetParameter::LoadAsync());
 		AudioAsset::Register(U"沈む。", Resource(U"resources/music/沈む。.mp3"), AssetParameter::LoadAsync());
 		AudioAsset::Register(U"かみさまのゆりかご", Resource(U"resources/music/かみさまのゆりかご.mp3"), AssetParameter::LoadAsync());
@@ -25,7 +26,7 @@ void Main()
 		AudioAsset::Register(U"かみさまのゆりかご", U"resources/music/かみさまのゆりかご.mp3", AssetParameter::LoadAsync());
 		AudioAsset::Register(U"真相探求", U"resources/music/真相探求.mp3", AssetParameter::LoadAsync());
 		AudioAsset::Register(U"安らぎと微睡み", U"resources/music/安らぎと微睡み.mp3", AssetParameter::LoadAsync());
-#endif
+#endif*/
 	}
 
 	// Cursor設定
