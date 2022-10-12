9caa5872ba5929f5bb2d7a7806795cfbea9cde60
sknjpn <sknjpn@gmail.com>
2022-10-13 02:59:45 +0900

    add websocketcode

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,68 +5,18 @@
 #include "WebSocket.hpp"
 #endif
 #ifdef SYLIFE_SERVER
-#include "lwsock.hpp"
-#include <thread>
-void worker(lwsock::WebSocket &&nws)
-{
-  auto hs = nws.recv_req(); // returned handshake data
-  nws.send_res();
-  std::string msg = "d e f";
-  nws.send_msg_txt(msg);
-  auto rcvd = nws.recv_msg_txt();
-  Console << Unicode::FromUTF8(rcvd.first);
-  // std::cout << rcvd << std::endl;
-  nws.send_close(1000);
-}
+#include "WebSocketServer.hpp"
 #endif
 
 void Main()
 {
 #ifdef SYLIFE_SERVER
-
-  lwsock::WebSocket s(lwsock::WebSocket::Mode::SERVER);
-  // s.ostream4log(cout);
-  s.bind("ws://localhost:3000");
-  s.listen(128);
-
-  std::thread th_server([&](){
-    while(true)
-    {
-      auto nws = s.accept();
-      auto hs = nws.recv_req();
-      Console << Unicode::FromUTF8(hs.first);
-      for(const auto& p : hs.second)
-      { 
-        Console << Unicode::FromUTF8(p.first);
-        Console << Unicode::FromUTF8(p.second);
-        Console << U"----";
-      }
-
-      nws.send_res();
-      nws.send_msg_txt("hello world!! from Server!!!");
-      auto rcvd = nws.recv_msg_txt();
-      Console << Unicode::FromUTF8(rcvd.first);
-      nws.send_close(1000); 
-    }
-  });
-
+  WebSocketServer wss(3000);
   while (System::Update())
   {
+    std::lock_guard<std::mutex> lock(wss.m_mtx);
     ClearPrint();
-    Print << U"Hello";
-  }
-
-  s.listen(5);
-  std::vector<std::thread> th_set;
-  for (int i = 0; i < 3; ++i)
-  {
-    lwsock::WebSocket nws = s.accept(); // blocking, return new WebSocket object
-    auto th = std::thread(worker, std::move(nws));
-    th_set.push_back(std::move(th));
-  }
-  for (auto &th : th_set)
-  {
-    th.join();
+    Print << wss.m_users.size();
   }
 #endif
 
---
+++ src/WebSocketServer.hpp
@@ -0,0 +1,74 @@
+#pragma once
+#include <Siv3D.hpp>
+#include "lwsock.hpp"
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <string>
+#include <memory>
+
+struct WebSocketServer
+{
+  struct User
+  {
+    User(const std::shared_ptr<lwsock::WebSocket>& conn)
+        : m_conn(conn)
+    {
+      auto hs = m_conn->recv_req();
+      Console << Unicode::FromUTF8(hs.first);
+      for (const auto &p : hs.second)
+      {
+        Console << Unicode::FromUTF8(p.first);
+        Console << Unicode::FromUTF8(p.second);
+        Console << U"----";
+      }
+      m_conn->send_res();
+
+      m_thread = std::thread([this]()
+                             {
+        while (true)
+        {
+          auto rcvd = m_conn->recv_msg_txt();
+          std::lock_guard<std::mutex> lock(m_mtx);
+          m_recv.emplace_back(rcvd.first);
+          Console << Unicode::FromUTF8(rcvd.first);
+        } });
+    }
+    ~User() { m_thread.join(); }
+
+    std::shared_ptr<lwsock::WebSocket> m_conn;
+    std::vector<std::string> m_recv;
+    std::thread m_thread;
+    std::mutex m_mtx;
+
+    void send(const std::string &text)
+    {
+      m_conn->send_msg_txt("hello world!! from Server!!!");
+    }
+  };
+
+  lwsock::WebSocket m_server;
+  std::thread m_thread;
+  std::vector<std::shared_ptr<User>> m_users;
+  std::mutex m_mtx;
+
+public:
+  WebSocketServer(int port)
+      : m_server(lwsock::WebSocket::Mode::SERVER)
+  {
+    m_server
+        .bind((std::string("ws://localhost:") + std::to_string(port)).c_str())
+        .listen(128);
+
+    m_thread = std::thread([this]()
+                           {
+  while (true)
+  {
+    auto user = std::make_shared<User>(std::make_shared<lwsock::WebSocket>(m_server.accept()));
+    std::lock_guard<std::mutex> lock(m_mtx);
+    m_users.emplace_back(user);
+  } });
+  }
+
+  ~WebSocketServer() { m_thread.join(); }
+};
